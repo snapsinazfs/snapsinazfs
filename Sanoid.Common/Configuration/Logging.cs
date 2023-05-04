9e83b7d0ae59b311b3b0f58b0eae89eb814f2bf7
Brandon Thetford <brandon@dodecatec.com>
2023-05-04 16:07:12 -0700

    Let's start off using nlog early on

---
+++ Sanoid.Common/Configuration/Logging.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace Sanoid.Common.Configuration
+{
+    /// <summary>
+    /// Configuration for logging using NLog
+    /// </summary>
+    public static class Logging
+    {
+        /// <summary>
+        /// Configures NLog using Sanoid.nlog.json
+        /// </summary>
+        public static void ConfigureLogger( )
+        {
+            IConfigurationRoot jsonConfigRoot = new ConfigurationManager()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Sanoid.nlog.json", false, true)
+                .Build();
+            LogManager.Configuration = new NLogLoggingConfiguration( jsonConfigRoot.GetSection( "NLog" ) );
+        }
+    }
+}
