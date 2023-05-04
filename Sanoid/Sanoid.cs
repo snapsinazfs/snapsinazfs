b8d5edebaaf0b9a9788a8e86b4613bebad184b92
Brandon Thetford <brandon@dodecatec.com>
2023-05-04 16:35:22 -0700

    Stub output for Sanoid

--- Sanoid/Sanoid.cs
+++ Sanoid/Sanoid.cs
@@ -1,4 +1,13 @@
-﻿using Sanoid.Common;
+﻿using NLog;
+using Sanoid.Common.Configuration;
 
-Console.WriteLine("Not yet implemented.");
-Console.WriteLine("Please use the Perl-based sanoid/syncoid for now.");
+Logging.ConfigureLogger();
+
+Logger Logger = NLog.LogManager.GetLogger("Sanoid");
+
+Logger.Info(Configuration.UseSanoidConfiguration ? "We are using sanoid.conf" : "We are not using sanoid.conf");
+
+Logger.Error("Not yet implemented.");
+Logger.Error("Please use the Perl-based sanoid/syncoid for now.");
+Logger.Error("This program will now exit with an error (status -1) to prevent accidental usage in scripts.");
+return -1;
