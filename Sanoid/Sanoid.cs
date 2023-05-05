833fd1b3d7080529be22fd4cad9c8b9400d9fd46
Brandon Thetford <brandon@dodecatec.com>
2023-05-05 02:00:28 -0700

    Follow naming standard

--- Sanoid/Sanoid.cs
+++ Sanoid/Sanoid.cs
@@ -6,7 +6,7 @@ using Sanoid.Common.Configuration;
 
 Logging.ConfigureLogger();
 
-Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
 ArgAction<CommandLineArguments> argParseReults = Args.InvokeMain<CommandLineArguments>( args );
 
@@ -15,8 +15,8 @@ if ( argParseReults.Cancelled )
     return 0;
 }
 
-Logger.Error( "Not yet implemented." );
-Logger.Error( "Please use the Perl-based sanoid/syncoid for now." );
-Logger.Error( "This program will now exit with an error (status -1) to prevent accidental usage in scripts." );
+logger.Error( "Not yet implemented." );
+logger.Error( "Please use the Perl-based sanoid/syncoid for now." );
+logger.Error( "This program will now exit with an error (status -1) to prevent accidental usage in scripts." );
 
 return -1;
