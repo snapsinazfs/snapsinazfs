9b6abf702be27860a3b2b00813f3f70113815a9e
Brandon Thetford <brandon@dodecatec.com>
2023-05-05 13:33:58 -0700

    Run project code cleanup rule before continuing

--- Sanoid.Common/Configuration/Configuration.cs
+++ Sanoid.Common/Configuration/Configuration.cs
@@ -1,45 +1,49 @@
+// /*
+// *  LICENSE:
+// *
+// *  This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
+// *  from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
+// *  project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
+// */
+
 using System.Text.Json.Serialization;
-
 using JetBrains.Annotations;
-
 using NLog;
 using NLog.Config;
 
 namespace Sanoid.Common.Configuration;
 
 /// <summary>
-/// Corresponds to the root section of Sanoid.json
+///     Corresponds to the root section of Sanoid.json
 /// </summary>
 public static class Configuration
 {
-    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
-
-    static Configuration( )
+    static Configuration()
     {
-        Log.Debug("Initializing root-level configuration from Sanoid.Json#/.");
+        Log.Debug( "Initializing root-level configuration from Sanoid.Json#/." );
         UseSanoidConfiguration = JsonConfigurationSections.RootConfiguration.GetBoolean( "UseSanoidConfiguration" );
-        SanoidConfigurationPathBase = JsonConfigurationSections.RootConfiguration[ "SanoidConfigurationPathBase" ] ?? "/etc/sanoid";
-        SanoidConfigurationDefaultsFile = JsonConfigurationSections.RootConfiguration[ "SanoidConfigurationDefaultsFile" ] ?? "sanoid.defaults.conf";
-        SanoidConfigurationLocalFile = JsonConfigurationSections.RootConfiguration[ "SanoidConfigurationLocalFile" ] ?? "sanoid.conf";
-        SanoidConfigurationCacheDirectory = JsonConfigurationSections.RootConfiguration[ "SanoidConfigurationCacheDirectory" ] ?? "/var/cache/sanoid";
-        SanoidConfigurationRunDirectory = JsonConfigurationSections.RootConfiguration[ "SanoidConfigurationRunDirectory" ] ?? "/var/run/sanoid";
-        Log.Debug("Root level configuration initialized.");
+        SanoidConfigurationPathBase = JsonConfigurationSections.RootConfiguration["SanoidConfigurationPathBase"] ?? "/etc/sanoid";
+        SanoidConfigurationDefaultsFile = JsonConfigurationSections.RootConfiguration["SanoidConfigurationDefaultsFile"] ?? "sanoid.defaults.conf";
+        SanoidConfigurationLocalFile = JsonConfigurationSections.RootConfiguration["SanoidConfigurationLocalFile"] ?? "sanoid.conf";
+        SanoidConfigurationCacheDirectory = JsonConfigurationSections.RootConfiguration["SanoidConfigurationCacheDirectory"] ?? "/var/cache/sanoid";
+        SanoidConfigurationRunDirectory = JsonConfigurationSections.RootConfiguration["SanoidConfigurationRunDirectory"] ?? "/var/run/sanoid";
+        Log.Debug( "Root level configuration initialized." );
     }
 
     /// <summary>
-    /// Gets or sets the default logging levels to be used by NLog
+    ///     Gets or sets the default logging levels to be used by NLog
     /// </summary>
     /// <remarks>
-    /// Getter returns the lowest severity logging level of all configured rules.<br/>
-    /// Setter overrides level for all configured rules.
+    ///     Getter returns the lowest severity logging level of all configured rules.<br />
+    ///     Setter overrides level for all configured rules.
     /// </remarks>
-    /// <value>A <see cref="LogLevel"/> indicating the lowest logging level set of any rule.</value>
+    /// <value>A <see cref="LogLevel" /> indicating the lowest logging level set of any rule.</value>
     public static LogLevel DefaultLoggingLevel
     {
         get
         {
             Log.Debug( "Getting lowest log level of all rules." );
-            LogLevel? lowestLogLevel = LogManager.Configuration!.LoggingRules.Min( rule => rule.Levels.Min( ) );
+            LogLevel? lowestLogLevel = LogManager.Configuration!.LoggingRules.Min( rule => rule.Levels.Min() );
             if ( lowestLogLevel is null )
             {
                 Log.Debug( "No logging levels set. Setting to {0}", LogLevel.Info.Name );
@@ -71,49 +75,33 @@ public static class Configuration
             }
 
             Log.Debug( "Reconfiguring loggers" );
-            LogManager.ReconfigExistingLoggers( );
+            LogManager.ReconfigExistingLoggers();
         }
     }
 
     /// <summary>
-    /// Gets or sets whether Sanoid.net should use ini-formatted configuration files using PERL sanoid's schema.<br />
-    /// Corresponds to the /UseSanoidConfiguration property of Sanoid.json.
+    ///     Gets or sets sanoid's cache path.<br />
+    ///     Corresponds to the /SanoidConfigurationCacheDirectory property of Sanoid.json.
     /// </summary>
     /// <remarks>
-    /// Default value is true<br />
-    /// If <c>true</c>, uses <see cref="SanoidConfigurationDefaultsFile"/> and <see cref="SanoidConfigurationLocalFile"/> in the <see cref="SanoidConfigurationPathBase"/> directory.<br />
-    /// If <c>false</c>, uses configuration in Sanoid.json only.
+    ///     Default value is "/var/cache/sanoid"<br />
     /// </remarks>
-    /// <value>A <see langword="bool"/> indicating whether PERL sanoid's configuration will be respected (<see langword="true"/>) or not (<see langword="false"/>).</value>
-    [JsonPropertyName( "UseSanoidConfiguration" )]
+    /// <value>A <see langword="string" /> indicating the path for sanoid-generated cache files</value>
+    [JsonPropertyName( "SanoidConfigurationCacheDirectory" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.Never )]
     [JsonRequired]
-    public static bool UseSanoidConfiguration { get; [NotNull] set; }
+    public static string SanoidConfigurationCacheDirectory { get; [NotNull] set; }
 
     /// <summary>
-    /// Gets or sets the absolute path to the directory containing PERL sanoid's configuration files.<br />
-    /// Corresponds to the /SanoidConfigurationPathBase property of Sanoid.json.
+    ///     Gets or sets the name of the ini-formatted file inside the <see cref="SanoidConfigurationPathBase" /> folder
+    ///     containing PERL sanoid's default configuration.<br />
+    ///     Corresponds to the /SanoidConfigurationDefaultsFile property of Sanoid.json.
     /// </summary>
     /// <remarks>
-    /// Default value is "/etc/sanoid"<br />
-    /// Should not contain a trailing slash.<br />
-    /// Not guaranteed to work with a relative path. Use an absolute path.
+    ///     Default value is "sanoid.defaults.conf"<br />
+    ///     Assumed to be a file name only.
     /// </remarks>
-    /// <value>A <see langword="string"/> indicating the absolute path to the directory containing PERL sanoid's configuration files</value>
-    [JsonPropertyName( "SanoidConfigurationPathBase" )]
-    [JsonIgnore( Condition = JsonIgnoreCondition.Never )]
-    [JsonRequired]
-    public static string SanoidConfigurationPathBase { get; [NotNull] set; }
-
-    /// <summary>
-    /// Gets or sets the name of the ini-formatted file inside the <see cref="SanoidConfigurationPathBase"/> folder containing PERL sanoid's default configuration.<br />
-    /// Corresponds to the /SanoidConfigurationDefaultsFile property of Sanoid.json.
-    /// </summary>
-    /// <remarks>
-    /// Default value is "sanoid.defaults.conf"<br />
-    /// Assumed to be a file name only.
-    /// </remarks>
-    /// <value>A <see langword="string"/> indicating the file name of the sanoid.defaults.conf file</value>
+    /// <value>A <see langword="string" /> indicating the file name of the sanoid.defaults.conf file</value>
     [JsonPropertyName( "SanoidConfigurationDefaultsFile" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.Never )]
     [JsonRequired]
@@ -121,43 +109,69 @@ public static class Configuration
 
 
     /// <summary>
-    /// Gets or sets the name of the ini-formatted file inside the <see cref="SanoidConfigurationPathBase"/> folder containing PERL sanoid's local configuration.<br />
-    /// Corresponds to the /SanoidConfigurationLocalFile property of Sanoid.json.
+    ///     Gets or sets the name of the ini-formatted file inside the <see cref="SanoidConfigurationPathBase" /> folder
+    ///     containing PERL sanoid's local configuration.<br />
+    ///     Corresponds to the /SanoidConfigurationLocalFile property of Sanoid.json.
     /// </summary>
     /// <remarks>
-    /// Default value is "sanoid.conf"<br />
-    /// Assumed to be a file name only.
+    ///     Default value is "sanoid.conf"<br />
+    ///     Assumed to be a file name only.
     /// </remarks>
-    /// <value>A <see langword="string"/> indicating the file name of the sanoid.conf file</value>
+    /// <value>A <see langword="string" /> indicating the file name of the sanoid.conf file</value>
     [JsonPropertyName( "SanoidConfigurationLocalFile" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.Never )]
     [JsonRequired]
     public static string SanoidConfigurationLocalFile { get; [NotNull] set; }
 
     /// <summary>
-    /// Gets or sets sanoid's cache path.<br />
-    /// Corresponds to the /SanoidConfigurationCacheDirectory property of Sanoid.json.
+    ///     Gets or sets the absolute path to the directory containing PERL sanoid's configuration files.<br />
+    ///     Corresponds to the /SanoidConfigurationPathBase property of Sanoid.json.
     /// </summary>
     /// <remarks>
-    /// Default value is "/var/cache/sanoid"<br />
+    ///     Default value is "/etc/sanoid"<br />
+    ///     Should not contain a trailing slash.<br />
+    ///     Not guaranteed to work with a relative path. Use an absolute path.
     /// </remarks>
-    /// <value>A <see langword="string"/> indicating the path for sanoid-generated cache files</value>
-    [JsonPropertyName( "SanoidConfigurationCacheDirectory" )]
+    /// <value>
+    ///     A <see langword="string" /> indicating the absolute path to the directory containing PERL sanoid's configuration
+    ///     files
+    /// </value>
+    [JsonPropertyName( "SanoidConfigurationPathBase" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.Never )]
     [JsonRequired]
-    public static string SanoidConfigurationCacheDirectory { get; [NotNull] set; }
+    public static string SanoidConfigurationPathBase { get; [NotNull] set; }
 
     /// <summary>
-    /// Gets or sets sanoid's run path.<br />
-    /// Corresponds to the /SanoidConfigurationRunDirectory property of Sanoid.json.
+    ///     Gets or sets sanoid's run path.<br />
+    ///     Corresponds to the /SanoidConfigurationRunDirectory property of Sanoid.json.
     /// </summary>
     /// <remarks>
-    /// Default value is "/var/run/sanoid"<br />
+    ///     Default value is "/var/run/sanoid"<br />
     /// </remarks>
-    /// <value>A <see langword="string"/> indicating the path for sanoid-generated runtime files</value>
+    /// <value>A <see langword="string" /> indicating the path for sanoid-generated runtime files</value>
     [JsonPropertyName( "SanoidConfigurationRunDirectory" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.Never )]
     [JsonRequired]
     public static string SanoidConfigurationRunDirectory { get; [NotNull] set; }
 
+    /// <summary>
+    ///     Gets or sets whether Sanoid.net should use ini-formatted configuration files using PERL sanoid's schema.<br />
+    ///     Corresponds to the /UseSanoidConfiguration property of Sanoid.json.
+    /// </summary>
+    /// <remarks>
+    ///     Default value is true<br />
+    ///     If <c>true</c>, uses <see cref="SanoidConfigurationDefaultsFile" /> and <see cref="SanoidConfigurationLocalFile" />
+    ///     in the <see cref="SanoidConfigurationPathBase" /> directory.<br />
+    ///     If <c>false</c>, uses configuration in Sanoid.json only.
+    /// </remarks>
+    /// <value>
+    ///     A <see langword="bool" /> indicating whether PERL sanoid's configuration will be respected (
+    ///     <see langword="true" />) or not (<see langword="false" />).
+    /// </value>
+    [JsonPropertyName( "UseSanoidConfiguration" )]
+    [JsonIgnore( Condition = JsonIgnoreCondition.Never )]
+    [JsonRequired]
+    public static bool UseSanoidConfiguration { get; [NotNull] set; }
+
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 }
