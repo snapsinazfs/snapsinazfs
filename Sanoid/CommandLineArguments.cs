028a901d1b37fd026e752f9c70662162a376e31f
Brandon Thetford <brandon@dodecatec.com>
2023-05-05 01:06:21 -0700

    Add the newly-created settings to argument parsing

--- Sanoid/CommandLineArguments.cs
+++ Sanoid/CommandLineArguments.cs
@@ -1,5 +1,7 @@
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
+using NLog;
 using PowerArgs;
+using Sanoid.Common.Configuration;
 
 namespace Sanoid
 {
@@ -11,10 +13,25 @@ namespace Sanoid
     internal class CommandLineArguments
     {
         [ArgDescription( "Verbose (Info level) output logging. Change log level in Sanoid.nlog.json for normal usage." )]
-        [ArgDescription( "Verbose output logging" )]
         [ArgShortcut( "v" )]
         [ArgShortcut( "--verbose" )]
+        [ArgCantBeCombinedWith( expression: "Debug|Quiet|ReallyQuiet" )]
         public bool Verbose { get; set; }
+
+        [ArgDescription( "Debug level output logging. Change log level in Sanoid.nlog.json for normal usage." )]
+        [ArgShortcut( "--debug" )]
+        [ArgCantBeCombinedWith( expression: "Verbose|Quiet|ReallyQuiet" )]
+        public bool Debug { get; set; }
+
+        [ArgDescription( "No output logging. Change log level to Off in Sanoid.nlog.json for normal usage. WILL WARN BEFORE SETTING IS APPLIED." )]
+        [ArgShortcut( "--quiet" )]
+        [ArgCantBeCombinedWith( expression: "Debug|Verbose" )]
+        public bool Quiet { get; set; }
+
+        [ArgDescription( "No output logging. Change log level to Off in Sanoid.nlog.json to set for normal usage. Will not warn when used." )]
+        [ArgShortcut( "--really-quiet" )]
+        [ArgCantBeCombinedWith( expression: "Debug|Verbose" )]
+        public bool ReallyQuiet { get; set; }
 
         [ArgDescription( "Shows this help" )]
         [ArgShortcut( "-h" )]
@@ -23,9 +40,8 @@ namespace Sanoid
         public bool Help { get; set; }
 
         [ArgDescription( "Forces loading of PERL sanoid's configuration files" )]
-        [ArgDescription( "Debug output logging" )]
-        [ArgShortcut( "--debug" )]
-        public bool Debug { get; set; }
+        [ArgShortcut( "--use-sanoid-config" )]
+        public bool UseSanoidConfig { get; set; }
 
         [ArgDescription( "Base configuration directory for sanoid" )]
         [ArgShortcut( "--configdir" )]
@@ -41,10 +57,42 @@ namespace Sanoid
         public string? RunDir { get; set; }
 
         /// <summary>
-        /// Stub method to allow PowerArgs to run without handing execution over to PowerArgs
+        /// Called by main thread to override configured settings with any arguments passed at the command line.
         /// </summary>
-        public void Main()
+        public void Main( )
         {
+            if ( Debug )
+            {
+                Configuration.DefaultLoggingLevel = LogLevel.Debug;
+            }
+            if ( Verbose )
+            {
+                Configuration.DefaultLoggingLevel = LogLevel.Info;
+            }
+            if ( Quiet )
+            {
+                Configuration.DefaultLoggingLevel = LogLevel.Off;
+            }
+            if ( ReallyQuiet )
+            {
+                LogManager.Configuration!.LoggingRules.ForEach( rule => rule.SetLoggingLevels( LogLevel.Off, LogLevel.Off ) );
+            }
+            if ( UseSanoidConfig )
+            {
+                Configuration.UseSanoidConfiguration = true;
+            }
+            if ( ConfigDir is not null )
+            {
+                Configuration.SanoidConfigurationPathBase = ConfigDir;
+            }
+            if ( CacheDir is not null )
+            {
+                Configuration.SanoidConfigurationCacheDirectory = CacheDir;
+            }
+            if ( RunDir is not null )
+            {
+                Configuration.SanoidConfigurationRunDirectory = RunDir;
+            }
         }
     }
 }
