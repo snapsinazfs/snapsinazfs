f5532055f1f820ab8b9042b062263df365757430
Brandon Thetford <brandon@dodecatec.com>
2023-05-05 20:00:38 -0700

    Don't re-validate the file every time we access this

--- Sanoid.Common/Configuration/JsonConfigurationSections.cs
+++ Sanoid.Common/Configuration/JsonConfigurationSections.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -41,10 +41,17 @@ public static class JsonConfigurationSections
     {
         get
         {
-            ValidateSanoidConfiguration();
-            return _rootConfiguration ??= new ConfigurationManager().AddJsonFile( "Sanoid.json", false, true ).Build();
+            if ( !ConfigIsValid )
+            {
+                ValidateSanoidConfiguration();
+                ConfigIsValid = true;
+            }
+
+            return _rootConfiguration ??= new ConfigurationManager( ).AddJsonFile( "Sanoid.json", false, true ).Build( );
         }
     }
+
+    private static bool ConfigIsValid;
 
     /// <summary>
     ///     Gets the /Formatting/SnapshotNaming configuration section of Sanoid.json
