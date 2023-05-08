5e95c983378865fe8ecc655617d9f90c5a4e8288
Brandon Thetford <brandon@dodecatec.com>
2023-05-07 22:35:51 -0700

    Add stub Dataset type

---
+++ Sanoid.Common/Configuration/Datasets/Dataset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanoid.Common.Configuration.Templates;
+
+namespace Sanoid.Common.Configuration.Datasets
+{
+    internal class Dataset
+    {
+        public Dataset( Template template )
+        {
+            Template = template;
+        }
+
+        internal Template Template { get; set; }
+
+        internal Template? TemplateOverrides { get; set; }
+    }
+}
