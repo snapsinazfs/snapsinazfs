341eadda0c6d30f660e98cdbb03b892077448426
Brandon Thetford <brandon@dodecatec.com>
2023-05-07 22:34:45 -0700

    Add types to support Templates

---
+++ Sanoid.Common/Configuration/Templates/SnapshotRetention.cs
@@ -0,0 +1,83 @@
+﻿// LICENSE:
+// 
+// This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
+// from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
+// project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
+
+using Microsoft.Extensions.Configuration;
+
+namespace Sanoid.Common.Configuration.Templates;
+
+/// <summary>
+///     Snapshot retention policies for use within <see cref="Template" />s
+/// </summary>
+public record struct SnapshotRetention
+{
+    /// <summary>
+    ///     Gets or sets how many daily snapshots will be retained
+    /// </summary>
+    public int Daily { get; set; }
+
+    /// <summary>
+    ///     Gets or sets how many frequent snapshots will be retained
+    /// </summary>
+    public int Frequent { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the interval, in minutes, between frequent snapshots
+    /// </summary>
+    /// <remarks>
+    ///     Should be a whole number factor of 60, such as 5, 10, 15, 20, or 30
+    /// </remarks>
+    public int FrequentPeriod { get; set; }
+
+    /// <summary>
+    ///     Gets or sets how many hourly snapshots will be retained
+    /// </summary>
+    public int Hourly { get; set; }
+
+    /// <summary>
+    ///     Gets or sets how many monthly snapshots will be retained
+    /// </summary>
+    public int Monthly { get; set; }
+
+    /// <summary>
+    ///     Gets or sets what percentage of remaining pool capacity must be reached before snapshots will be pruned by this
+    ///     policy
+    /// </summary>
+    public int PruneDeferral { get; set; }
+
+    /// <summary>
+    ///     Gets or sets how many weekly snapshots will be retained
+    /// </summary>
+    public int Weekly { get; set; }
+
+    /// <summary>
+    ///     Gets or sets how many yearly snapshots will be retained
+    /// </summary>
+    public int Yearly { get; set; }
+
+    /// <summary>
+    ///     Gets a new immutable <see cref="SnapshotRetention" /> record, parsed from an <see cref="IConfiguration" /> object
+    /// </summary>
+    /// <param name="config">
+    ///     A reference to an <see cref="IConfiguration" /> object containing a single instance of a snapshot
+    ///     retention policy.
+    /// </param>
+    /// <returns>
+    ///     A new immutable <see cref="SnapshotRetention" /> record, parsed from <paramref name="config" />
+    /// </returns>
+    public static SnapshotRetention FromConfiguration( IConfiguration config )
+    {
+        return new SnapshotRetention
+        {
+            Daily = config.GetInt( "Daily" ),
+            Frequent = config.GetInt( "Frequent" ),
+            FrequentPeriod = config.GetInt( "FrequentPeriod" ),
+            Hourly = config.GetInt( "Hourly" ),
+            Monthly = config.GetInt( "Monthly" ),
+            PruneDeferral = config.GetInt( "PruneDeferral" ),
+            Yearly = config.GetInt( "Yearly" )
+        };
+    }
+}
---
+++ Sanoid.Common/Configuration/Templates/SnapshotTiming.cs
@@ -0,0 +1,100 @@
+﻿// LICENSE:
+// 
+// This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
+// from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
+// project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
+
+using Microsoft.Extensions.Configuration;
+
+namespace Sanoid.Common.Configuration.Templates;
+
+/// <summary>
+///     Snapshot timing policies for use in <see cref="Template" />s
+/// </summary>
+public record struct SnapshotTiming
+{
+    /// <summary>
+    ///     Gets or sets the time of day that daily snapshots are taken
+    /// </summary>
+    public TimeOnly DailyTime { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the minute of the hour that hourly snapshots are taken
+    /// </summary>
+    public int HourlyMinute { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the day of the month that monthly snapshots are taken
+    /// </summary>
+    /// <remarks>
+    ///     If the current month has fewer days than the specified value, monthly snapshots will be taken on the last day of
+    ///     the month
+    /// </remarks>
+    public int MonthlyDay { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the time of day that monthly snapshots are taken
+    /// </summary>
+    public TimeOnly MonthlyTime { get; set; }
+
+    /// <summary>
+    ///     Gets or sets whether local time is used for snapshot naming and processing.
+    /// </summary>
+    public bool UseLocalTime { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the day of the week on which weekly snapshots are taken
+    /// </summary>
+    public DayOfWeek WeeklyDay { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the time of day that weekly snapshots are taken
+    /// </summary>
+    public TimeOnly WeeklyTime { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the day of the <see cref="YearlyMonth" /> that yearly snapshots are taken
+    /// </summary>
+    /// <remarks>
+    ///     If the current month has fewer days than the specified value, yearly snapshots will be taken on the last day of
+    ///     <see cref="YearlyMonth" />
+    /// </remarks>
+    public int YearlyDay { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the month of the year in which yearly snapshots will be taken
+    /// </summary>
+    public int YearlyMonth { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the time of day that yearly snapshots are taken
+    /// </summary>
+    public TimeOnly YearlyTime { get; set; }
+
+    /// <summary>
+    ///     Gets a new immutable <see cref="SnapshotTiming" /> record, parsed from an <see cref="IConfiguration" /> object
+    /// </summary>
+    /// <param name="config">
+    ///     A reference to an <see cref="IConfiguration" /> object containing a single instance of a snapshot
+    ///     timing policy.
+    /// </param>
+    /// <returns>
+    ///     A new immutable <see cref="SnapshotTiming" /> record, parsed from <paramref name="config" />
+    /// </returns>
+    public static SnapshotTiming FromConfiguration( IConfiguration config )
+    {
+        return new SnapshotTiming
+        {
+            DailyTime = TimeOnly.Parse( config[ "DailyTime" ] ?? "00:00:00" ),
+            HourlyMinute = config.GetInt( "HourlyMinute" ),
+            MonthlyDay = config.GetInt( "MonthlyDay" ),
+            MonthlyTime = TimeOnly.Parse( config[ "MonthlyTime" ] ?? "00:00:00" ),
+            UseLocalTime = config.GetBoolean( "UseLocalTime", true ),
+            WeeklyDay = (DayOfWeek)config.GetInt( "WeeklyDay", 1 ),
+            WeeklyTime = TimeOnly.Parse( config[ "WeeklyTime" ] ?? "00:00:00" ),
+            YearlyDay = config.GetInt( "YearlyDay", 31 ),
+            YearlyMonth = config.GetInt( "YearlyMonth" ),
+            YearlyTime = TimeOnly.Parse( config[ "YearlyTime" ] ?? "00:00:00" )
+        };
+    }
+}
