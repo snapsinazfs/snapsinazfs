fb799de2b85493b1d3f89c53d68eadf909fe4bb3
Brandon Thetford <brandon@dodecatec.com>
2023-05-08 18:10:07 -0700

    This needs to be public

--- Sanoid.Common/Configuration/Datasets/Dataset.cs
+++ Sanoid.Common/Configuration/Datasets/Dataset.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -8,7 +8,10 @@ using Sanoid.Common.Configuration.Templates;
 
 namespace Sanoid.Common.Configuration.Datasets;
 
-internal class Dataset
+/// <summary>
+///     Represents Dataset configuration in Sanoid.json
+/// </summary>
+public class Dataset
 {
     public Dataset( Template template )
     {
