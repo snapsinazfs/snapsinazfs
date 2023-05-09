bef8eba84fc2e45e763b9bb377a6ee7656aadaec
Brandon Thetford <brandon@dodecatec.com>
2023-05-09 15:27:12 -0700

    WTH was all that? Just do this quick and easy.

--- Sanoid.Common/Zfs/CommandRunner.cs
+++ Sanoid.Common/Zfs/CommandRunner.cs
@@ -4,40 +4,43 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
-using System.Collections.Concurrent;
 using System.Diagnostics;
 using Sanoid.Common.Configuration;
-using Sanoid.Common.Configuration.Datasets;
 
 namespace Sanoid.Common.Zfs;
 
 internal static class CommandRunner
 {
-    private static ConcurrentDictionary<string, Dataset> _allDatasets = new( );
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger( );
 
-    internal static void ZfsListAll( )
+    internal static List<string> ZfsListAll( )
     {
+        List<string> dataSets = new( );
         ProcessStartInfo zfsListStartInfo = new( JsonConfigurationSections.PlatformUtilitiesConfiguration[ "zfs" ]!, "list -o name -t filesystem,volume -Hr" )
         {
             CreateNoWindow = true,
-            RedirectStandardOutput = true,
-            UseShellExecute = true
+            RedirectStandardOutput = true
         };
-        using ( SemaphoreSlim zfsListWaitSemaphore = new( 1, 1 ) )
+        using ( Process zfsListProcess = new( ) { StartInfo = zfsListStartInfo } )
         {
-            using ( Process? zfsListProcess = new( ) { EnableRaisingEvents = true, StartInfo = zfsListStartInfo } )
+            Logger.Debug( "Calling {0} {1}", zfsListStartInfo.FileName, zfsListStartInfo.Arguments );
+            zfsListProcess.Start( );
+            while ( !zfsListProcess.StandardOutput.EndOfStream )
             {
-                zfsListProcess.OutputDataReceived += ZfsListProcess_OutputDataReceived;
-                zfsListProcess.Exited += ( sender, args ) => { zfsListWaitSemaphore.Release( ); };
-                zfsListProcess.Start( );
-                zfsListWaitSemaphore.Wait( 30000 );
+                string outputLine = zfsListProcess.StandardOutput.ReadLine( )!;
+                Logger.Trace( "{0}", outputLine );
+                dataSets.Add( outputLine! );
             }
-        }
-    }
 
-    private static void ZfsListProcess_OutputDataReceived( object sender, DataReceivedEventArgs e )
-    {
-        Logger.Debug( "{0}", e.Data );
+            if ( !zfsListProcess.HasExited )
+            {
+                Logger.Trace( "Waiting for zfs list process to exit" );
+                zfsListProcess.WaitForExit( 3000 );
+            }
+
+            Logger.Debug( "zfs list process finished" );
+        }
+
+        return dataSets;
     }
 }
