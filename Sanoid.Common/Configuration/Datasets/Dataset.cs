1f2874533cca396dcc0f0e1b702cda5d7d08896b
Brandon Thetford <brandon@dodecatec.com>
2023-05-10 21:35:45 -0700

    Formatting and removal of unused property

--- Sanoid.Common/Configuration/Datasets/Dataset.cs
+++ Sanoid.Common/Configuration/Datasets/Dataset.cs
@@ -4,7 +4,6 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
-using System.Text.Json.Serialization;
 using Sanoid.Common.Configuration.Templates;
 
 namespace Sanoid.Common.Configuration.Datasets;
@@ -23,14 +22,22 @@ public class Dataset
         Path = path;
     }
 
-    private Dataset? _parent;
+    private readonly Dataset? _parent;
 
     /// <summary>
     ///     Gets a collection of all child Datasets, indexed by their ZFS paths.
     /// </summary>
     public Dictionary<string, Dataset> Children { get; } = new( );
 
+    /// <summary>
+    ///     Gets or sets if this Dataset is enabled for explicit processing or not.
+    /// </summary>
     public bool Enabled { get; set; }
+
+    /// <summary>
+    ///     Gets or sets whether this dataset exists explicitly in the configuration
+    /// </summary>
+    public bool IsInConfiguration { get; set; }
 
     /// <summary>
     ///     Gets or sets the parent of this Dataset
@@ -54,28 +61,30 @@ public class Dataset
     /// </summary>
     public string Path { get; }
 
-    internal string VirtualPath => $"/{Path}";
-
     /// <summary>
-    ///     Gets or sets whether this dataset exists explicitly in the configuration
-    /// </summary>
-    public bool IsInConfiguration { get; set; }
-
-    /// <summary>
-    ///     Gets the root <see cref="Dataset"/>, which is a dummy Dataset that serves as the single root for all ZFS pools.
+    ///     Gets the root <see cref="Dataset" />, which is a dummy Dataset that serves as the single root for all ZFS pools.
     /// </summary>
     /// <value>
-    ///     A <see cref="Dataset"/> with no parent, no overrides, the default template, and set as disabled.
+    ///     A <see cref="Dataset" /> with no parent, no overrides, the default template, and set as disabled.
     /// </value>
     public static Dataset Root { get; } = new( "/" )
     {
         Template = Template.GetDefault( ),
         Enabled = false,
-        Parent = null,
-        TemplateOverrides = null
+        Parent = null
     };
 
+    /// <summary>
+    ///     Gets or sets the <see cref="Templates.Template" /> this <see cref="Dataset" /> will use.
+    /// </summary>
+    /// <value>
+    ///     A <see cref="Templates.Template" /> that this Dataset will use, or <see langword="null" /> if it has not yet been
+    ///     set.
+    /// </value>
     public Template? Template { get; set; }
 
-    internal Template? TemplateOverrides { get; set; }
+    /// <summary>
+    ///     Gets the <see cref="Path" /> of this Dataset, prepended with a slash, to represent its location in the virtual tree
+    /// </summary>
+    internal string VirtualPath => $"/{Path}";
 }
