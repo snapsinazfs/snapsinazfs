c6f274dae82f6283a935ea3f64d04a7df8bbc9de
Brandon Thetford <brandon@dodecatec.com>
2023-05-10 17:55:00 -0700

    Add property specifying if a dataset was explicitly configured

--- Sanoid.Common/Configuration/Datasets/Dataset.cs
+++ Sanoid.Common/Configuration/Datasets/Dataset.cs
@@ -54,6 +54,11 @@ public class Dataset
     public string Path { get; set; }
 
     /// <summary>
+    ///     Gets or sets whether this dataset exists explicitly in the configuration
+    /// </summary>
+    public bool IsInConfiguration { get; set; }
+
+    /// <summary>
     ///     Gets the virtual path of the dataset in the tree, which is just the ZFS path prefixed with a leading slash
     /// </summary>
     public string TreeVirtualPath => $"/{Path}";
