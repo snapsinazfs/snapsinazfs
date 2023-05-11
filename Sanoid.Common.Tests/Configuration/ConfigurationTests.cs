1de1655bb4fe62b93542228d2da0e14db8708dff
Brandon Thetford <brandon@dodecatec.com>
2023-05-11 14:10:59 -0700

    Not a valid test

--- Sanoid.Common.Tests/Configuration/ConfigurationTests.cs
+++ Sanoid.Common.Tests/Configuration/ConfigurationTests.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -105,11 +105,5 @@ public class ConfigurationTests
                 Assert.Fail( $"{key} does not exist in Sanoid.json" );
             }
         }
-    }
-
-    [Test]
-    [Order( 2 )]
-    public void AnotherTest( )
-    {
     }
 }
