fd07015550bf538154aa40eb4c7812b9b4615a94
Brandon Thetford <brandon@dodecatec.com>
2023-05-23 17:20:12 -0700

    Add a static class to handle Sanoid.net OS mutexes
    We'll use this instead of lockfiles to provide process isolation.

---
+++ Sanoid.Interop/Concurrency/Mutexes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanoid.Interop.Concurrency;
+public static class Mutexes
+{
+    private static Mutex? _sanoidMutex;
+    public static Mutex GetSanoidMutex(out bool createdNew )
+    {
+        createdNew = false;
+        return _sanoidMutex ??= new( true, "Global\\Sanoid.net", out createdNew );
+    }
+}
