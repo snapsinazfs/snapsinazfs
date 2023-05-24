41012db3df8b0175384dc8199190eaa19b049e4e
Brandon Thetford <brandon@dodecatec.com>
2023-05-24 03:42:14 -0700

    Add tests to ensure the runtime will generate the expected strings for zfs object types

---
+++ Sanoid.Common.Tests/Zfs/ZfsCommandRunnerTests.cs
@@ -0,0 +1,27 @@
+﻿// LICENSE:
+// 
+// This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
+// from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
+// project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
+
+using Sanoid.Common.Zfs;
+
+namespace Sanoid.Common.Tests.Zfs;
+
+[TestFixture]
+public class ZfsCommandRunnerTests
+{
+    [Test( Description = "Tests all possible values of ZfsListObjectTypes and their expected string representations" )]
+    [Category( "General" )]
+    [TestCase( (ZfsListObjectTypes)1, ExpectedResult = "filesystem" )]
+    [TestCase( (ZfsListObjectTypes)2, ExpectedResult = "snapshot" )]
+    [TestCase( (ZfsListObjectTypes)3, ExpectedResult = "filesystem,snapshot" )]
+    [TestCase( (ZfsListObjectTypes)4, ExpectedResult = "volume" )]
+    [TestCase( (ZfsListObjectTypes)5, ExpectedResult = "filesystem,volume" )]
+    [TestCase( (ZfsListObjectTypes)6, ExpectedResult = "snapshot,volume" )]
+    [TestCase( (ZfsListObjectTypes)7, ExpectedResult = "filesystem,snapshot,volume" )]
+    public string ListTypeEnumStringsAsExpected( ZfsListObjectTypes types )
+    {
+        return types.ToStringForCommandLine( );
+    }
+}
