42d163e8d9c06c3e87246ec352f677e2d40692d5
Brandon Thetford <brandon@dodecatec.com>
2023-05-24 02:06:48 -0700

    Don't need the static constructor

--- Sanoid.Interop/Concurrency/Mutexes.cs
+++ Sanoid.Interop/Concurrency/Mutexes.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -13,10 +13,6 @@ namespace Sanoid.Interop.Concurrency;
 
 public sealed class Mutexes : IDisposable
 {
-    static Mutexes( )
-    {
-    }
-
     private Mutexes( )
     {
         Logger.Debug( "Creating mutex manager." );
