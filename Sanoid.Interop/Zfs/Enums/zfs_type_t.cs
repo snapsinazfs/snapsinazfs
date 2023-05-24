b223d73a17acbe3209a8e9e9e35331d6ff9103b3
Brandon Thetford <brandon@dodecatec.com>
2023-05-24 02:40:13 -0700

    Silence naming rule warnings because we're about to use this

--- Sanoid.Interop/Zfs/Enums/zfs_type_t.cs
+++ Sanoid.Interop/Zfs/Enums/zfs_type_t.cs
@@ -4,8 +4,13 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Sanoid.Interop.Zfs.Enums;
 
+#pragma warning disable IDE1006 // Naming Styles
+[SuppressMessage( "ReSharper", "InconsistentNaming", Justification = "Keeping names as defined in libzfs, per recommendations for native types used with P/Invoke")]
+[Flags]
 public enum zfs_type_t
 {
     ZFS_TYPE_INVALID = 0,
@@ -16,3 +21,4 @@ public enum zfs_type_t
     ZFS_TYPE_BOOKMARK = 1 << 4,
     ZFS_TYPE_VDEV = 1 << 5
 }
+#pragma warning restore IDE1006 // Naming Styles
