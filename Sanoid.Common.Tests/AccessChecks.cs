205561797677e12abea137dc19f61466d0a8edd5
Brandon Thetford <brandon@dodecatec.com>
2023-05-24 19:52:59 -0700

    I suppose it makes more sense to just restrict it to Unix

--- Sanoid.Common.Tests/AccessChecks.cs
+++ Sanoid.Common.Tests/AccessChecks.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -13,7 +13,7 @@ using Sanoid.Interop.Libc.Enums;
 namespace Sanoid.Common.Tests;
 
 [TestFixture]
-[Platform( Exclude = "WIN32NT" )]
+[Platform( Include = "Unix" )]
 [Order( 100 )]
 [NonParallelizable]
 [Category( "General" )]
