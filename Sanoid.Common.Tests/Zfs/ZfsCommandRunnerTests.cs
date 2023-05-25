25f1e7586ed182cd485ad97fc70058d958b251d1
Brandon Thetford <brandon@dodecatec.com>
2023-05-24 18:42:15 -0700

    Add a couple of checks to enforce structure of this enum

--- Sanoid.Common.Tests/Zfs/ZfsCommandRunnerTests.cs
+++ Sanoid.Common.Tests/Zfs/ZfsCommandRunnerTests.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -11,6 +11,24 @@ namespace Sanoid.Common.Tests.Zfs;
 [TestFixture]
 public class ZfsCommandRunnerTests
 {
+    [Test]
+    [Category( "General" )]
+    [Category( "TypeChecks" )]
+    public void ZfsListObjectTypesHasFlagsAttribute( )
+    {
+        Assert.That( typeof( ZfsListObjectTypes ), Has.Attribute<FlagsAttribute>( ) );
+    }
+
+    [Test]
+    [Category( "General" )]
+    [Category( "TypeChecks" )]
+    [Sequential]
+    public void ZfsListObjectTypesOnlyExpectedValuesDefined( )
+    {
+        int[] expectedValues = new[] { 1, 2, 4 };
+        Assert.That( Enum.GetValuesAsUnderlyingType<ZfsListObjectTypes>( ), Is.EquivalentTo( expectedValues ) );
+    }
+
     [Test( Description = "Tests all possible values of ZfsListObjectTypes and their expected string representations" )]
     [Category( "General" )]
     [TestCase( (ZfsListObjectTypes)1, "filesystem" )]
