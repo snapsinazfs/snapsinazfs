e946807c9c6b244846a6bcbcc2c6e71fff2a3763
Brandon Thetford <brandon@dodecatec.com>
2023-05-28 02:47:09 -0700

    Add extension for conversion of string to DatasetKind

---
+++ Sanoid.Interop/Zfs/ZfsTypes/DatasetKindExtensions.cs
@@ -0,0 +1,20 @@
+﻿// LICENSE:
+// 
+// This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
+// from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
+// project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
+
+namespace Sanoid.Interop.Zfs.ZfsTypes;
+
+public static class DatasetKindExtensions
+{
+    public static DatasetKind ToDatasetKind( this string value )
+    {
+        return value switch
+        {
+            "filesystem" => DatasetKind.FileSystem,
+            "volume" => DatasetKind.Volume,
+            _ => throw new NotSupportedException( $"String value {value} not supported for conversion to DatasetKind" );
+        };
+    }
+}
