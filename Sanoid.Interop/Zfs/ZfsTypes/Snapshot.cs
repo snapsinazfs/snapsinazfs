23ef791dbee81d001b18f9c1ea62b9d05129d94f
Brandon Thetford <brandon@dodecatec.com>
2023-05-29 01:57:59 -0700

    Add Jetbrains.Annotations

--- Sanoid.Interop/Zfs/ZfsTypes/Snapshot.cs
+++ Sanoid.Interop/Zfs/ZfsTypes/Snapshot.cs
@@ -4,30 +4,45 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
-using System.Collections.Concurrent;
-
 namespace Sanoid.Interop.Zfs.ZfsTypes;
 
 /// <summary>
 ///     A ZFS snapshot
 /// </summary>
-public class Snapshot : IZfsObject
+public class Snapshot : ZfsObjectBase
 {
     /// <summary>
-    ///     Creates a new <see cref="Snapshot" /> with the given name
+    ///     Creates a new <see cref="Snapshot" /> with the given name and an empty <see cref="ZfsObjectBase.Properties" />
+    ///     collection
     /// </summary>
     /// <param name="name">The final component of the name of the Snapshot</param>
-    public Snapshot(string name)
+    public Snapshot( string name )
+        : base( name, ZfsObjectKind.Snapshot )
     {
-        Name = name;
     }
 
-    /// <inheritdoc />
-    public string Name { get; }
+    private Snapshot( string parentPath, string name )
+        : base( name, ZfsObjectKind.Snapshot )
+    {
+        ParentPath = parentPath;
+    }
 
-    /// <inheritdoc />
-    public ZfsObjectKind ZfsKind { get; }
+    public static Snapshot Invalid => new( "@@INVALID@@" )
+    {
+        Properties =
+        {
+            [ "sanoid.net:invalid" ] = new( "sanoid.net:", "invalid", "true", "sanoid" )
+        }
+    };
 
-    /// <inheritdoc />
-    public ConcurrentDictionary<string, ZfsProperty> Properties { get; private set; }
+    /// <summary>
+    ///     Gets or sets the path of the parent object, without trailing '/'
+    /// </summary>
+    /// <remarks>
+    ///     Is not validated
+    /// </remarks>
+    /// <value>
+    ///     <see langword="null" /> or a string representing the path of the <see cref="Dataset" /> the snapshot belongs to
+    /// </value>
+    public string? ParentPath { get; set; }
 }
