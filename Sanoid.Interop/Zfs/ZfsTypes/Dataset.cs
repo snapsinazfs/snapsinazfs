e2eea3ab08167e30488b8e6bbdd44c885a4786f9
Brandon Thetford <brandon@dodecatec.com>
2023-05-29 03:40:21 -0700

    Add properties that are currently defined

--- Sanoid.Interop/Zfs/ZfsTypes/Dataset.cs
+++ Sanoid.Interop/Zfs/ZfsTypes/Dataset.cs
@@ -4,8 +4,8 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
-using Sanoid.Interop.Zfs.ZfsCommandRunner;
 
 namespace Sanoid.Interop.Zfs.ZfsTypes;
 
@@ -19,11 +19,66 @@ public class Dataset : ZfsObjectBase
     /// </summary>
     /// <param name="name">The name of the new <see cref="Dataset" /></param>
     /// <param name="kind">The <see cref="DatasetKind" /> of Dataset to create</param>
-    /// <param name="validateName">Whether to validate the name of the new <see cref="Dataset"/> (<see langword="true"/>) or not (<see langword="false"/> - default)</param>
-    /// <param name="validatorRegex">The <see cref="Regex"/> to user for name validation</param>
-    public Dataset( string name, DatasetKind kind, bool validateName = false,Regex? validatorRegex = null )
+    /// <param name="validateName">
+    ///     Whether to validate the name of the new <see cref="Dataset" /> (<see langword="true" />) or
+    ///     not (<see langword="false" /> - default)
+    /// </param>
+    /// <param name="validatorRegex">The <see cref="Regex" /> to user for name validation</param>
+    public Dataset( string name, DatasetKind kind, bool validateName = false, Regex? validatorRegex = null )
         : base( name, (ZfsObjectKind)kind, validatorRegex, validateName )
     {
-
     }
+
+    [JsonIgnore]
+    public bool AutoPrune
+    {
+        get
+        {
+            string valueString = Properties.TryGetValue( "sanoid.net:autoprune", out ZfsProperty? prop ) ? prop.Value : "false";
+            return bool.TryParse( valueString, out bool result ) && result;
+        }
+    }
+
+    [JsonIgnore]
+    public bool AutoSnapshot
+    {
+        get
+        {
+            string valueString = Properties.TryGetValue( "sanoid.net:autosnapshot", out ZfsProperty? prop ) ? prop.Value : "false";
+            return bool.TryParse( valueString, out bool result ) && result;
+        }
+    }
+
+    [JsonIgnore]
+    public bool Enabled
+    {
+        get
+        {
+            string valueString = Properties.TryGetValue( "sanoid.net:enabled", out ZfsProperty? prop ) ? prop.Value : "false";
+            return bool.TryParse( valueString, out bool result ) && result;
+        }
+    }
+
+    [JsonIgnore]
+    public bool Recursive
+    {
+        get
+        {
+            string valueString = Properties.TryGetValue( "sanoid.net:recursive", out ZfsProperty? prop ) ? prop.Value : "false";
+            return bool.TryParse( valueString, out bool result ) && result;
+        }
+    }
+
+    [JsonIgnore]
+    public bool SkipChildren
+    {
+        get
+        {
+            string valueString = Properties.TryGetValue( "sanoid.net:skipchildren", out ZfsProperty? prop ) ? prop.Value : "false";
+            return bool.TryParse( valueString, out bool result ) && result;
+        }
+    }
+
+    [JsonIgnore]
+    public string Template => Properties.TryGetValue( "sanoid.net:template", out ZfsProperty? prop ) ? prop.Value : "default";
 }
