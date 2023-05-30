41d9452bf4a7b0bd5d051a027e6125cfb23c4427
Brandon Thetford <brandon@dodecatec.com>
2023-05-29 18:47:40 -0700

    Bring back the Dummy command runner for dev on windows

---
+++ Sanoid.Interop/Zfs/ZfsCommandRunner/DummyZfsCommandRunner.cs
@@ -0,0 +1,30 @@
+﻿// LICENSE:
+// 
+// This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
+// from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
+// project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
+
+using Sanoid.Interop.Zfs.ZfsTypes;
+
+namespace Sanoid.Interop.Zfs.ZfsCommandRunner;
+
+internal class DummyZfsCommandRunner : ZfsCommandRunnerBase
+{
+    /// <inheritdoc />
+    public override bool TakeSnapshot( string snapshotName )
+    {
+        return true;
+    }
+
+    /// <inheritdoc />
+    public override Dictionary<string, ZfsProperty> GetZfsProperties( ZfsObjectKind kind, string zfsObjectName, bool sanoidOnly = true )
+    {
+        return new( );
+    }
+
+    /// <inheritdoc />
+    public override bool SetZfsProperty( string zfsPath, params ZfsProperty[] properties )
+    {
+        return true;
+    }
+}
