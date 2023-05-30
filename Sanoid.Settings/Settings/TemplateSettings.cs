edeb654253977a9b0ecd34969dc3017baa74f915
Brandon Thetford <brandon@dodecatec.com>
2023-05-30 00:29:51 -0700

    Allow templates to override naming

--- Sanoid.Settings/Settings/TemplateSettings.cs
+++ Sanoid.Settings/Settings/TemplateSettings.cs
@@ -4,6 +4,8 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
+using System.Text.Json.Serialization;
+
 namespace Sanoid.Settings.Settings;
 
 /// <summary>
@@ -11,6 +13,9 @@ namespace Sanoid.Settings.Settings;
 /// </summary>
 public sealed class TemplateSettings
 {
+    [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+    public FormattingSettings? Formatting { get; set; }
+
     /// <summary>
     ///     Gets or sets the global PruneSnapshots setting
     /// </summary>
@@ -30,4 +35,19 @@ public sealed class TemplateSettings
     ///     Gets or sets the global TakeSnapshots setting
     /// </summary>
     public required bool TakeSnapshots { get; set; }
+
+    /// <summary>
+    ///     Gets formatting settings for this template, or the fallback settings provided
+    /// </summary>
+    /// <param name="fallbackSettings">Fallback settings to use if this template doesn't have a Formatting section.</param>
+    public FormattingSettings GetFormattingSettings( FormattingSettings fallbackSettings )
+    {
+        return Formatting ?? fallbackSettings;
+    }
+
+    public string GenerateFullSnapshotName( string datasetName, SnapshotPeriod period, DateTimeOffset timestamp, FormattingSettings fallbackSettings )
+    {
+        Formatting ??= fallbackSettings;
+        return Formatting.GenerateFullSnapshotName( datasetName, period, timestamp );
+    }
 }
