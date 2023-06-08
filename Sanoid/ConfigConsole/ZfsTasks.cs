9ae7d180f555ce3935ece71bae116d0df0b05a9a
Brandon Thetford <brandon@dodecatec.com>
2023-06-07 18:39:34 -0700

    Wrong separator character for zfs get

--- Sanoid/ConfigConsole/ZfsTasks.cs
+++ Sanoid/ConfigConsole/ZfsTasks.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -26,7 +26,7 @@ internal static class ZfsTasks
             } );
         }
 
-        await foreach ( string zfsLine in commandRunner.ZfsExecEnumerator( "get", $"type,{string.Join( ',', ZfsProperty.KnownDatasetProperties )} -Hprt filesystem,volume {string.Join( ',', datasets.Keys )}" ).ConfigureAwait( true ) )
+        await foreach ( string zfsLine in commandRunner.ZfsExecEnumerator( "get", $"type,{string.Join( ',', ZfsProperty.KnownDatasetProperties )} -Hprt filesystem,volume {string.Join( ' ', datasets.Keys )}" ).ConfigureAwait( true ) )
         {
             string[] lineTokens = zfsLine.Split( '\t', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries );
             datasets.AddOrUpdate( lineTokens[ 0 ], k =>
