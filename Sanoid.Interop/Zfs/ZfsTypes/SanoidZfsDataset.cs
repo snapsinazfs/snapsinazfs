73aee49992d6efdb02573b863718f4099c0943a5
Brandon Thetford <brandon@dodecatec.com>
2023-06-09 22:31:34 -0700

    Add an indexer to get properties by name

--- Sanoid.Interop/Zfs/ZfsTypes/SanoidZfsDataset.cs
+++ Sanoid.Interop/Zfs/ZfsTypes/SanoidZfsDataset.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -125,6 +125,23 @@ public record SanoidZfsDataset( string Name, string Kind, bool IsPoolRoot, TreeN
                 return SnapshotRetentionPruneDeferral = SnapshotRetentionPruneDeferral with { Value = propertyValue, Source = propertySource };
             default:
                 throw new ArgumentOutOfRangeException( nameof( propertyName ), $"{propertyName} is not a supported int property" );
+        }
+    }
+
+    public IZfsProperty this[ string propName ]
+    {
+        get
+        {
+            ArgumentException.ThrowIfNullOrEmpty( propName );
+            return propName switch
+            {
+                ZfsPropertyNames.EnabledPropertyName => Enabled,
+                ZfsPropertyNames.TakeSnapshotsPropertyName => TakeSnapshots,
+                ZfsPropertyNames.PruneSnapshotsPropertyName => PruneSnapshots,
+                ZfsPropertyNames.RecursionPropertyName => Recursion,
+                ZfsPropertyNames.TemplatePropertyName => Template,
+                _ => throw new ArgumentOutOfRangeException( nameof( propName ) )
+            };
         }
     }
 }
