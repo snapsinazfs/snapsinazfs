9a171df0a70dadd0b97f72325e5237ccf0fd52b8
Brandon Thetford <brandon@dodecatec.com>
2023-06-12 20:04:57 -0700

    Add an extension method for day names

--- Sanoid/ConfigConsole/TypeExtensions.cs
+++ Sanoid/ConfigConsole/TypeExtensions.cs
@@ -4,6 +4,8 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using Sanoid.Interop.Zfs.ZfsTypes;
 using Terminal.Gui;
 
@@ -48,5 +50,36 @@ public static class TypeExtensions
     public static bool GetSelectedBooleanFromLabel( this RadioGroup group )
     {
         return bool.Parse( group.RadioLabels[ group.SelectedItem ].ToString( ) ?? throw new InvalidOperationException( "Failed getting radio group boolean value" ) );
+    }
+
+    /// <summary>
+    ///     Gets a list of strings containing day names
+    /// </summary>
+    /// <param name="value">A <see cref="DateTimeFormatInfo" /> object to get day names for</param>
+    /// <returns>
+    ///     A <see cref="List{T}" /> of <see langword="string" />s representing all long and standard abbreviated forms
+    ///     of the names of the days of the week, in the culture of this <see cref="DateTimeFormatInfo" />,
+    ///     in culture-specific order.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="value" /> or any of its required members are <see langword="null" />.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">If the <see cref="DateTimeFormatInfo.DayNames"/> or <see cref="DateTimeFormatInfo.AbbreviatedDayNames"/> properties of <paramref name="value"/> are not 7-element <see langword="string"/> arrays</exception>
+    [Pure]
+    [SuppressMessage( "ReSharper", "ExceptionNotDocumentedOptional", Justification = "The only other exceptions that can be thrown here are for setters that we do not use")]
+    public static List<string> GetLongAndAbbreviatedDayNames( this DateTimeFormatInfo value )
+    {
+        ArgumentNullException.ThrowIfNull( value );
+        if ( value.DayNames is not string[] { Length: 7 } )
+        {
+            throw new InvalidOperationException( "Invalid DayNames collection in DateTimeFormatInfo object" );
+        }
+        if ( value.AbbreviatedDayNames is not string[] { Length: 7 } )
+        {
+            throw new InvalidOperationException( "Invalid AbbreviatedDayNames collection in DateTimeFormatInfo object" );
+        }
+        List<string> dayNamesList = new( value.DayNames );
+        dayNamesList.AddRange( value.AbbreviatedDayNames );
+        return dayNamesList;
     }
 }
