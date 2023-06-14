d59002438ee7ae8958606323f1aa5398b9854aaa
Brandon Thetford <brandon@dodecatec.com>
2023-06-13 19:50:21 -0700

    Add an extension for cleaner conversion of TimeSpan to TimeOnly

--- Sanoid/ConfigConsole/TypeExtensions.cs
+++ Sanoid/ConfigConsole/TypeExtensions.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -178,4 +178,16 @@ public static class TypeExtensions
             _ => null
         };
     }
+
+    /// <summary>
+    /// A shortcut to calling <see cref="TimeOnly.FromTimeSpan"/> on <paramref name="value"/>
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns>The time portion of <paramref name="value"/></returns>
+    [Pure]
+    public static TimeOnly ToTimeOnly( this TimeSpan value )
+    {
+        return TimeOnly.FromTimeSpan( value );
+    }
+
 }
