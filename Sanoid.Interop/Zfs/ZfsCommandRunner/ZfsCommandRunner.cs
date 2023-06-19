dce0596bfbaf568c7f91f711153f105a175fb3ef
Brandon Thetford <brandon@dodecatec.com>
2023-06-18 21:53:14 -0700

    Insert -r argument if dataset is configured for zfs recursion

--- Sanoid.Interop/Zfs/ZfsCommandRunner/ZfsCommandRunner.cs
+++ Sanoid.Interop/Zfs/ZfsCommandRunner/ZfsCommandRunner.cs
@@ -1,4 +1,4 @@
-// LICENSE:
+﻿// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
@@ -88,7 +88,7 @@ public class ZfsCommandRunner : ZfsCommandRunnerBase, IZfsCommandRunner
             return false;
         }
 
-        string arguments = $"snapshot {string.Join( ' ', snapshot.Properties.Values.Select( p => $"-o {p.SetString} " ) )} {snapshot.Name}";
+        string arguments = $"snapshot {((ds.Recursion == ZfsPropertyValueConstants.ZfsRecursion) ? "-r " : "")}{string.Join( ' ', snapshot.Properties.Values.Select( p => $"-o {p.SetString} " ) )} {snapshot.Name}";
         ProcessStartInfo zfsSnapshotStartInfo = new( PathToZfsUtility, arguments )
         {
             CreateNoWindow = true,
