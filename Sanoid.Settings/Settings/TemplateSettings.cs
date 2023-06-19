ab15191095c1134707d2dcde42feb587fab46651
Brandon Thetford <brandon@dodecatec.com>
2023-06-18 22:55:03 -0700

    XmlDoc

--- Sanoid.Settings/Settings/TemplateSettings.cs
+++ Sanoid.Settings/Settings/TemplateSettings.cs
@@ -4,13 +4,19 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
+using JetBrains.Annotations;
+
 namespace Sanoid.Settings.Settings;
 
 /// <summary>
 ///     Settings definitions for templates
 /// </summary>
+[UsedImplicitly( ImplicitUseKindFlags.Access, ImplicitUseTargetFlags.WithMembers )]
 public record TemplateSettings
 {
+    /// <summary>
+    ///     Gets or sets the Formatting sub-section for this <see cref="TemplateSettings" /> object
+    /// </summary>
     public FormattingSettings Formatting { get; set; } = FormattingSettings.GetDefault( );
 
     /// <summary>
@@ -18,6 +24,7 @@ public record TemplateSettings
     /// </summary>
     public SnapshotTimingSettings SnapshotTiming { get; set; } = SnapshotTimingSettings.GetDefault( );
 
+    /// <inheritdoc cref="FormattingSettings.GenerateFullSnapshotName" />
     public string GenerateFullSnapshotName( string datasetName, SnapshotPeriodKind periodKind, DateTimeOffset timestamp )
     {
         return Formatting.GenerateFullSnapshotName( datasetName, periodKind, timestamp );
