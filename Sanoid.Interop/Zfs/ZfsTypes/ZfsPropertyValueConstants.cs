02302548fb2255a13893c009d821757a0386c323
Brandon Thetford <brandon@dodecatec.com>
2023-06-19 00:41:05 -0700

    Add a collection to check property ranges against

--- Sanoid.Interop/Zfs/ZfsTypes/ZfsPropertyValueConstants.cs
+++ Sanoid.Interop/Zfs/ZfsTypes/ZfsPropertyValueConstants.cs
@@ -4,6 +4,8 @@
 // from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
 // project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Sanoid.Interop.Zfs.ZfsTypes;
 
 public static class ZfsPropertyValueConstants
@@ -12,4 +14,15 @@ public static class ZfsPropertyValueConstants
     public const string None = "-";
     public const string Sanoid = "sanoid";
     public const string ZfsRecursion = "zfs";
+
+    public static readonly Dictionary<string, (int Min, int Max)> IntPropertyRanges = new( )
+    {
+        { ZfsPropertyNames.SnapshotRetentionFrequentPropertyName, new( 0, int.MaxValue ) },
+        { ZfsPropertyNames.SnapshotRetentionHourlyPropertyName, new( 0, int.MaxValue ) },
+        { ZfsPropertyNames.SnapshotRetentionDailyPropertyName, new( 0, int.MaxValue ) },
+        { ZfsPropertyNames.SnapshotRetentionWeeklyPropertyName, new( 0, int.MaxValue ) },
+        { ZfsPropertyNames.SnapshotRetentionMonthlyPropertyName, new( 0, int.MaxValue ) },
+        { ZfsPropertyNames.SnapshotRetentionYearlyPropertyName, new( 0, int.MaxValue ) },
+        { ZfsPropertyNames.SnapshotRetentionPruneDeferralPropertyName, new( 0, 100 ) }
+    };
 }
