fe7c380170176a56aacfe8ee9000404f86516035
Brandon Thetford <github@snapsinazfs.com>
2023-06-26 18:03:55 -0700

    Add methods to parse some ZfsProperty<T> types from RawProperty values

--- SnapsInAZfs.Interop/Zfs/ZfsTypes/ZfsProperty.cs
+++ SnapsInAZfs.Interop/Zfs/ZfsTypes/ZfsProperty.cs
@@ -2,7 +2,9 @@
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
+using SnapsInAZfs.Interop.Zfs.ZfsCommandRunner;
 using SnapsInAZfs.Settings.Settings;
 
 namespace SnapsInAZfs.Interop.Zfs.ZfsTypes;
@@ -47,4 +49,112 @@ public readonly record struct ZfsProperty<T>( string Name, T Value, string Sourc
 
     [JsonIgnore]
     public string SetString => $"{Name}={ValueString}";
+
+    /// <summary>
+    ///     Attempts to parse a <see cref="RawProperty" /> as its <see cref="ZfsProperty{T}" /> (<see langword="bool" />) equivalent
+    /// </summary>
+    /// <param name="input">The <see cref="RawProperty" /> to parse</param>
+    /// <param name="property">
+    ///     The parsed <see cref="ZfsProperty{T}" /> (<see langword="bool" />), if successful
+    /// </param>
+    /// <returns>
+    ///     <see langword="true" /> if <paramref name="input" /> was parsed successfully; otherwise <see langword="false" />
+    /// </returns>
+    /// <remarks>
+    ///     <paramref name="property" /> is never null when this method returns <see langword="true" />; otherwise,
+    ///     <paramref name="property" /> is always <see langword="null" />
+    /// </remarks>
+    public static bool TryParse( RawProperty input, [NotNullWhen( true )] out ZfsProperty<bool>? property )
+    {
+        property = null;
+
+        if ( bool.TryParse( input.Value, out bool result ) )
+        {
+            property = new( input.Name, result, input.Source );
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    ///     Attempts to parse a <see cref="RawProperty" /> as its <see cref="ZfsProperty{T}" /> (<see langword="int" />) equivalent
+    /// </summary>
+    /// <param name="input">The <see cref="RawProperty" /> to parse</param>
+    /// <param name="property">
+    ///     The parsed <see cref="ZfsProperty{T}" /> (<see langword="int" />), if successful
+    /// </param>
+    /// <returns>
+    ///     <see langword="true" /> if <paramref name="input" /> was parsed successfully; otherwise <see langword="false" />
+    /// </returns>
+    /// <remarks>
+    ///     <paramref name="property" /> is never null when this method returns <see langword="true" />; otherwise,
+    ///     <paramref name="property" /> is always <see langword="null" />
+    /// </remarks>
+    public static bool TryParse( RawProperty input, [NotNullWhen( true )] out ZfsProperty<int>? property )
+    {
+        if ( int.TryParse( input.Value, out int result ) )
+        {
+            property = new ZfsProperty<int>( input.Name, result, input.Source );
+            return true;
+        }
+
+        property = null;
+        return false;
+    }
+
+    /// <summary>
+    ///     Attempts to parse a <see cref="RawProperty" /> as its <see cref="ZfsProperty{T}" /> (<see cref="DateTimeOffset" />)
+    ///     equivalent
+    /// </summary>
+    /// <param name="input">The <see cref="RawProperty" /> to parse</param>
+    /// <param name="property">
+    ///     The parsed <see cref="ZfsProperty{T}" /> (<see cref="DateTimeOffset" />), if successful
+    /// </param>
+    /// <returns>
+    ///     <see langword="true" /> if <paramref name="input" /> was parsed successfully; otherwise <see langword="false" />
+    /// </returns>
+    /// <remarks>
+    ///     <paramref name="property" /> is never null when this method returns <see langword="true" />; otherwise,
+    ///     <paramref name="property" /> is always <see langword="null" />
+    /// </remarks>
+    public static bool TryParse( RawProperty input, [NotNullWhen( true )] out ZfsProperty<DateTimeOffset>? property )
+    {
+        if ( DateTimeOffset.TryParse( input.Value, out DateTimeOffset result ) )
+        {
+            property = new ZfsProperty<DateTimeOffset>( input.Name, result, input.Source );
+            return true;
+        }
+
+        property = null;
+        return false;
+    }
+
+    /// <summary>
+    ///     Attempts to parse a <see cref="RawProperty" /> as its <see cref="ZfsProperty{T}" /> (<see cref="SnapshotPeriod" />)
+    ///     equivalent
+    /// </summary>
+    /// <param name="input">The <see cref="RawProperty" /> to parse</param>
+    /// <param name="property">
+    ///     The parsed <see cref="ZfsProperty{T}" /> (<see cref="SnapshotPeriod" />), if successful
+    /// </param>
+    /// <returns>
+    ///     <see langword="true" /> if <paramref name="input" /> was parsed successfully; otherwise <see langword="false" />
+    /// </returns>
+    /// <remarks>
+    ///     <paramref name="property" /> is never null when this method returns <see langword="true" />; otherwise,
+    ///     <paramref name="property" /> is always <see langword="null" />
+    /// </remarks>
+    public static bool TryParse( RawProperty input, [NotNullWhen( true )] out ZfsProperty<SnapshotPeriod>? property )
+    {
+        SnapshotPeriod period = (SnapshotPeriod)input.Value;
+        if ( period.Kind is not SnapshotPeriodKind.Manual and not SnapshotPeriodKind.Temporary )
+        {
+            property = new ZfsProperty<SnapshotPeriod>( input.Name, period, input.Source );
+            return true;
+        }
+
+        property = null;
+        return false;
+    }
 }
