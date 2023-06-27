283956026a69c7818e731a81ffbe283a3825d1c8
Brandon Thetford <github@snapsinazfs.com>
2023-06-26 17:25:42 -0700

    Add a single-shot constructor for ZfsRecord

--- SnapsInAZfs.Interop/Zfs/ZfsTypes/ZfsRecord.cs
+++ SnapsInAZfs.Interop/Zfs/ZfsTypes/ZfsRecord.cs
@@ -1,4 +1,4 @@
-﻿// LICENSE:
+// LICENSE:
 // 
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license
 
@@ -29,6 +29,53 @@ public record ZfsRecord
             ZfsPropertyValueConstants.Snapshot => ZfsIdentifierRegexes.SnapshotNameRegex( ),
             _ => throw new InvalidOperationException( "Unknown type of object specified for ZfsIdentifierValidator." )
         };
+    }
+
+    public ZfsRecord( string name, string kind, ZfsProperty<bool> enabled, ZfsProperty<bool> takeSnapshots, ZfsProperty<bool> pruneSnapshots, ZfsProperty<DateTimeOffset> lastFrequentSnapshotTimestamp, ZfsProperty<DateTimeOffset> lastHourlySnapshotTimestamp, ZfsProperty<DateTimeOffset> lastDailySnapshotTimestamp, ZfsProperty<DateTimeOffset> lastWeeklySnapshotTimestamp, ZfsProperty<DateTimeOffset> lastMonthlySnapshotTimestamp, ZfsProperty<DateTimeOffset> lastYearlySnapshotTimestamp, ZfsProperty<string> recursion, ZfsProperty<string> template, ZfsProperty<int> retentionFrequent, ZfsProperty<int> retentionHourly, ZfsProperty<int> retentionDaily, ZfsProperty<int> retentionWeekly, ZfsProperty<int> retentionMonthly, ZfsProperty<int> retentionYearly, ZfsProperty<int> retentionPruneDeferral, ZfsRecord? parent = null )
+        : this( name, kind, parent )
+    {
+        Enabled = enabled;
+        TakeSnapshots = takeSnapshots;
+        PruneSnapshots = pruneSnapshots;
+        if ( lastFrequentSnapshotTimestamp.IsLocal )
+        {
+            LastFrequentSnapshotTimestamp = lastFrequentSnapshotTimestamp;
+        }
+
+        if ( lastHourlySnapshotTimestamp.IsLocal )
+        {
+            LastHourlySnapshotTimestamp = lastHourlySnapshotTimestamp;
+        }
+
+        if ( lastDailySnapshotTimestamp.IsLocal )
+        {
+            LastDailySnapshotTimestamp = lastDailySnapshotTimestamp;
+        }
+
+        if ( lastWeeklySnapshotTimestamp.IsLocal )
+        {
+            LastWeeklySnapshotTimestamp = lastWeeklySnapshotTimestamp;
+        }
+
+        if ( lastMonthlySnapshotTimestamp.IsLocal )
+        {
+            LastMonthlySnapshotTimestamp = lastMonthlySnapshotTimestamp;
+        }
+
+        if ( lastYearlySnapshotTimestamp.IsLocal )
+        {
+            LastYearlySnapshotTimestamp = lastYearlySnapshotTimestamp;
+        }
+
+        Recursion = recursion;
+        Template = template;
+        SnapshotRetentionFrequent = retentionFrequent;
+        SnapshotRetentionHourly = retentionHourly;
+        SnapshotRetentionDaily = retentionDaily;
+        SnapshotRetentionWeekly = retentionWeekly;
+        SnapshotRetentionMonthly = retentionMonthly;
+        SnapshotRetentionYearly = retentionYearly;
+        SnapshotRetentionPruneDeferral = retentionPruneDeferral;
     }
 
     public ZfsProperty<bool> Enabled { get; private set; } = new( ZfsPropertyNames.EnabledPropertyName, false, "local" );
