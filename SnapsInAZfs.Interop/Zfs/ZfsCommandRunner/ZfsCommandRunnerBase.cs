1124361009c721bbedf94d2ca2942c2e03546132
Brandon Thetford <github@snapsinazfs.com>
2023-07-08 04:04:10 -0700

    These functions are no longer used

--- SnapsInAZfs.Interop/Zfs/ZfsCommandRunner/ZfsCommandRunnerBase.cs
+++ SnapsInAZfs.Interop/Zfs/ZfsCommandRunner/ZfsCommandRunnerBase.cs
@@ -224,69 +224,6 @@ public abstract class ZfsCommandRunnerBase : IZfsCommandRunner
         allDatasets[ dsName ].UpdateProperty( propertyName, propertyValue, propertySource );
     }
 
-    protected static void ParseDatasetZfsGetLineForConfigConsoleTree( ConcurrentDictionary<string, ZfsRecord> baseDatasets, ConcurrentDictionary<string, ZfsRecord> treeDatasets, string[] lineTokens, ConcurrentDictionary<string, TreeNode> allTreeNodes )
-    {
-        //TODO: This can likely be reduced to the config console-specific parts, to make use of the new zfs output parsing logic
-        string dsName = lineTokens[ 0 ];
-        string propertyValue = lineTokens[ 2 ];
-        if ( !baseDatasets.ContainsKey( dsName ) )
-        {
-            Logger.Trace( "{0} is not in dictionary. Creating a new {1}", dsName, propertyValue );
-            string parentName = dsName.GetZfsPathParent( );
-            bool isPoolRoot = dsName == parentName;
-            ZfsRecord parentDsBaseCopy = baseDatasets[ parentName ];
-            ZfsRecord parentDsTreeCopy = treeDatasets[ parentName ];
-            ZfsRecord newDsBaseCopy = new( dsName, propertyValue, isPoolRoot ? null : parentDsBaseCopy );
-            ZfsRecord newDsTreeCopy = newDsBaseCopy with { ParentDataset = parentDsTreeCopy };
-            ZfsObjectConfigurationTreeNode node = new( dsName, newDsBaseCopy, newDsTreeCopy, parentDsBaseCopy, parentDsTreeCopy );
-            allTreeNodes[ dsName ] = node;
-            allTreeNodes[ parentName ].Children.Add( node );
-            Logger.Debug( "Adding new {0} {1} to {2}", newDsBaseCopy.Kind, newDsBaseCopy.Name, parentDsBaseCopy.Name );
-            baseDatasets.TryAdd( dsName, newDsBaseCopy );
-            treeDatasets.TryAdd( dsName, newDsTreeCopy );
-        }
-        else
-        {
-            ZfsRecord ds = baseDatasets[ dsName ];
-            if ( ds.IsPoolRoot )
-            {
-                Logger.Trace( "{0} is a pool root - skipping", dsName );
-                return;
-            }
-
-            string propertyName = lineTokens[ 1 ];
-            string propertySource = lineTokens[ 3 ];
-            Logger.Debug( "Adding property {0} ({1}) - ({2}) to {3}", propertyName, propertyValue, propertySource, ds.Name );
-            ds.UpdateProperty( propertyName, propertyValue, propertySource );
-            treeDatasets[ ds.Name ].UpdateProperty( propertyName, propertyValue, propertySource );
-        }
-    }
-
-    protected static void ParsePoolRootDatasetZfsGetLineForConfigConsoleTree( ConcurrentDictionary<string, ZfsRecord> baseDatasets, ConcurrentDictionary<string, ZfsRecord> treeDatasets, string[] lineTokens, List<ITreeNode> treeRootNodes, ConcurrentDictionary<string, TreeNode> allTreeNodes )
-    {
-        string propertyValue = lineTokens[ 2 ];
-        string dsName = lineTokens[ 0 ];
-        baseDatasets.AddOrUpdate( dsName, k =>
-        {
-            ZfsRecord newRootDsBaseCopy = new( k, propertyValue );
-            ZfsRecord newRootDsTreeCopy = newRootDsBaseCopy with { };
-            ZfsObjectConfigurationTreeNode node = new( dsName, newRootDsBaseCopy, newRootDsTreeCopy );
-            Logger.Debug( "Adding new pool root object {0} to collections", newRootDsBaseCopy.Name );
-            treeRootNodes.Add( node );
-            allTreeNodes[ dsName ] = node;
-            treeDatasets.TryAdd( k, newRootDsTreeCopy );
-            return newRootDsBaseCopy;
-        }, ( _, ds ) =>
-        {
-            string propertyName = lineTokens[ 1 ];
-            string propertySource = lineTokens[ 3 ];
-            ds.UpdateProperty( propertyName, propertyValue, propertySource );
-            treeDatasets[ dsName ].UpdateProperty( propertyName, propertyValue, propertySource );
-            Logger.Debug( "Updating property {0} for {1} to {2}", propertyName, dsName, propertyValue );
-            return ds;
-        } );
-    }
-
     protected static void ProcessRawObjects( SortedDictionary<string, RawZfsObject> rawObjects, ConcurrentDictionary<string, ZfsRecord> datasets, ConcurrentDictionary<string, Snapshot> snapshots )
     {
         foreach ( ( string objName, RawZfsObject obj ) in rawObjects )
