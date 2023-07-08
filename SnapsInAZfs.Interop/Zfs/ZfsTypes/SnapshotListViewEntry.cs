7ea211b1fda77d7fc1555c32c40d16a149ecb54a
Brandon Thetford <github@snapsinazfs.com>
2023-07-08 03:14:37 -0700

    Add a new type for use in the config console for snapshots

---
+++ SnapsInAZfs.Interop/Zfs/ZfsTypes/SnapshotListViewEntry.cs
@@ -0,0 +1,19 @@
+﻿// LICENSE:
+// 
+// This software is licensed for use under the Free Software Foundation's GPL v3.0 license
+
+namespace SnapsInAZfs.Interop.Zfs.ZfsTypes;
+
+public record SnapshotListViewEntry( string ListViewText, Snapshot BaseSnapshot, Snapshot ListViewSnapshot )
+{
+    public bool IsModified => BaseSnapshot != ListViewSnapshot;
+    public Snapshot ListViewSnapshot { get; private set; } = ListViewSnapshot;
+
+    /// <inheritdoc />
+    public override string ToString( ) => ListViewText;
+
+    public void ResetSnapshot( )
+    {
+        ListViewSnapshot = BaseSnapshot with { };
+    }
+}
