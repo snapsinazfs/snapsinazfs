a0c926fffbce210fdd3effc7db6654a77189c3d9
Brandon Thetford <github@snapsinazfs.com>
2023-07-08 04:04:48 -0700

    Undo the rename in this

--- SnapsInAZfs.Interop/Zfs/ZfsTypes/ZfsObjectConfigurationTreeNode.cs
+++ SnapsInAZfs.Interop/Zfs/ZfsTypes/ZfsObjectConfigurationTreeNode.cs
@@ -8,18 +8,18 @@ namespace SnapsInAZfs.Interop.Zfs.ZfsTypes;
 
 public class ZfsObjectConfigurationTreeNode : TreeNode
 {
-    public ZfsObjectConfigurationTreeNode( string name, ZfsRecord baseObject, ZfsRecord treeObject )
+    public ZfsObjectConfigurationTreeNode( string name, ZfsRecord baseDataset, ZfsRecord treeDataset )
         : base( name )
     {
-        TreeObject = treeObject;
-        BaseObject = baseObject;
-        TreeSnapshots = TreeObject.Snapshots.Values.SelectMany( periodCollection => periodCollection.Values, ( _, snap ) => new SnapshotListViewEntry( snap.Name, snap, snap with { } ) ).ToList( );
-        BaseSnapshots = BaseObject.Snapshots.Values.SelectMany( periodCollection => periodCollection.Values, ( _, snap ) => new SnapshotListViewEntry( snap.Name, snap, snap with { } ) ).ToList( );
+        TreeDataset = treeDataset;
+        BaseDataset = baseDataset;
+        TreeSnapshots = TreeDataset.Snapshots.Values.SelectMany( periodCollection => periodCollection.Values, ( _, snap ) => new SnapshotListViewEntry( snap.Name, snap, snap with { } ) ).ToList( );
+        BaseSnapshots = BaseDataset.Snapshots.Values.SelectMany( periodCollection => periodCollection.Values, ( _, snap ) => new SnapshotListViewEntry( snap.Name, snap, snap with { } ) ).ToList( );
     }
 
-    public ZfsRecord BaseObject { get; set; }
+    public ZfsRecord BaseDataset { get; set; }
     public List<SnapshotListViewEntry> BaseSnapshots { get; set; }
-    public bool IsModified => TreeObject != BaseObject;
-    public ZfsRecord TreeObject { get; set; }
+    public bool IsModified => TreeDataset != BaseDataset;
+    public ZfsRecord TreeDataset { get; set; }
     public List<SnapshotListViewEntry> TreeSnapshots { get; set; }
 }
