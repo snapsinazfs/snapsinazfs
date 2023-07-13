97e196ff0bf7700731375dfe8db097ae172631b3
Brandon Thetford <github@snapsinazfs.com>
2023-07-13 09:23:54 -0700

    Wire up the ZfsTask to handle inheritance
    Do it asynchronously and return success or failure based on all operations succeeding or one failing

--- SnapsInAZfs/ConfigConsole/ZfsTasks.cs
+++ SnapsInAZfs/ConfigConsole/ZfsTasks.cs
@@ -4,6 +4,7 @@
 
 using System.Collections.Concurrent;
 using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
 using SnapsInAZfs.ConfigConsole.TreeNodes;
 using SnapsInAZfs.Interop.Zfs.ZfsCommandRunner;
 using SnapsInAZfs.Interop.Zfs.ZfsTypes;
@@ -19,6 +20,26 @@ internal static class ZfsTasks
     public static bool SetPropertiesForDataset( bool dryRun, string zfsPath, List<IZfsProperty> modifiedProperties, IZfsCommandRunner commandRunner )
     {
         return commandRunner.SetZfsProperties( dryRun, zfsPath, modifiedProperties );
+    }
+
+    public static async Task<bool> InheritPropertiesForDataset( bool dryRun, string zfsPath, List<IZfsProperty> inheritedProperties, IZfsCommandRunner commandRunner )
+    {
+        int successfulOperations = 0;
+        List<Task> zfsInheritTasks = new( );
+        foreach ( IZfsProperty property in inheritedProperties )
+        {
+            zfsInheritTasks.Add( commandRunner.InheritZfsPropertyAsync( dryRun, zfsPath, property ).ContinueWith( inheritTask => {
+                // ReSharper disable once AsyncConverter.AsyncWait
+                if ( inheritTask.Result )
+                {
+                    Interlocked.Increment( ref successfulOperations );
+                }
+            } ) );
+        }
+
+        await Task.WhenAll( zfsInheritTasks ).ConfigureAwait( true );
+
+        return successfulOperations == inheritedProperties.Count;
     }
 
     internal static async Task<List<ITreeNode>> GetFullZfsConfigurationTreeAsync( SnapsInAZfsSettings settings, ConcurrentDictionary<string, ZfsRecord> baseDatasets, ConcurrentDictionary<string, ZfsRecord> treeDatasets, ConcurrentDictionary<string, Snapshot> baseSnapshots, IZfsCommandRunner commandRunner )
