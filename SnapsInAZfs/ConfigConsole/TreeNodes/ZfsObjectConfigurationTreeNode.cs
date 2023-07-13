a1e37b608d73e9df73e1a086db7a172cbe43be07
Brandon Thetford <github@snapsinazfs.com>
2023-07-13 01:29:03 -0700

    Rework this to be simpler

--- SnapsInAZfs/ConfigConsole/TreeNodes/ZfsObjectConfigurationTreeNode.cs
+++ SnapsInAZfs/ConfigConsole/TreeNodes/ZfsObjectConfigurationTreeNode.cs
@@ -3,7 +3,6 @@
 // This software is licensed for use under the Free Software Foundation's GPL v3.0 license
 
 using System.Collections.Concurrent;
-
 using SnapsInAZfs.Interop.Zfs.ZfsTypes;
 using Terminal.Gui.Trees;
 
@@ -11,122 +10,189 @@ namespace SnapsInAZfs.ConfigConsole.TreeNodes;
 
 public class ZfsObjectConfigurationTreeNode : TreeNode
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger( );
+
+    private readonly ConcurrentDictionary<string, IZfsProperty> _modifiedPropertiesSinceLastSave = new( );
+
     public ZfsObjectConfigurationTreeNode( string name, ZfsRecord baseDataset, ZfsRecord treeDataset )
         : base( name )
     {
         TreeDataset = treeDataset;
         BaseDataset = baseDataset;
-        TreeSnapshots = TreeDataset.Snapshots.Values.SelectMany( periodCollection => periodCollection.Values, ( _, snap ) => new SnapshotListViewEntry( snap.Name, snap, snap with { } ) ).ToList( );
-        BaseSnapshots = BaseDataset.Snapshots.Values.SelectMany( periodCollection => periodCollection.Values, ( _, snap ) => new SnapshotListViewEntry( snap.Name, snap, snap with { } ) ).ToList( );
+    }
+
+    private List<ITreeNode>? _children;
+
+    /// <summary>
+    ///     The base instance of the <see cref="ZfsRecord" />.
+    /// </summary>
+    /// <remarks>
+    ///     This object remains unmodified until the user saves an object.<br />
+    ///     At that point, <see cref="CopyTreeDatasetPropertiesToBaseDataset" /> is called to copy the changes to this object.
+    /// </remarks>
+    public ZfsRecord BaseDataset { get; }
+
+    /// <inheritdoc />
+    public override IList<ITreeNode> Children
+    {
+        get
+        {
+            List<ITreeNode> list = new( );
+            foreach ( ( string? childName, ZfsRecord? child ) in TreeDataset.GetSortedChildDatasets( ) )
+            {
+                ITreeNode node = new ZfsObjectConfigurationTreeNode( childName.GetLastPathElement( ), BaseDataset.GetChild( childName ), child );
+                list.Add( node );
+            }
+
+            return _children ??= list;
+        }
     }
 
     /// <summary>
-    /// Gets or sets the base (unmodified) <see cref="ZfsRecord"/> for this entry.
+    ///     Gets a boolean value indicating if any properties have been changed in the UI for this specific node, and not just due to
+    ///     inheritance from a modified parent.
     /// </summary>
-    public ZfsRecord BaseDataset { get; private set; }
-    public List<SnapshotListViewEntry> BaseSnapshots { get; set; }
-    private readonly ConcurrentDictionary<string, IZfsProperty> _modifiedPropertiesSinceLastSaveForCurrentItem = new( );
+    public bool IsLocallyModified => _modifiedPropertiesSinceLastSave.Count > 0;
 
-    public void ResetTreeDataset( )
-    {
-        RestorePreviousSelectedZfsTreeNode( );
-        _modifiedPropertiesSinceLastSaveForCurrentItem.Clear( );
-    }
+    /// <summary>
+    ///     Gets a boolean value indicating if <see cref="TreeDataset" /> is not equal to <see cref="BaseDataset" />
+    /// </summary>
+    public bool IsModified => !TreeDataset.Equals( BaseDataset );
 
-    public void CopyTreeDatasetToBaseDataset( )
+    /// <inheritdoc />
+    public override string Text
     {
-        BaseDataset = TreeDataset.DeepCopyClone( BaseDataset.IsPoolRoot ? null : BaseDataset.ParentDataset );
+        get => TreeDataset.Name.GetLastPathElement( );
+        set => Logger.Warn( "Illegal attempt to set text of a tree node to {0}", value );
     }
 
     /// <summary>
-    /// Gets a boolean value indicating if <see cref="TreeDataset"/> is not equal to <see cref="BaseDataset"/>
+    ///     The tree representation of the <see cref="ZfsRecord" />.
     /// </summary>
-    public bool IsModified => TreeDataset != BaseDataset;
+    /// <remarks>
+    ///     This is the object that gets modified by changes to the input fields.<br />
+    ///     When an object is saved, the changes are copied to <see cref="BaseDataset" />
+    /// </remarks>
+    public ZfsRecord TreeDataset { get; }
 
-    public ZfsRecord TreeDataset { get; private set; }
-    public List<SnapshotListViewEntry> TreeSnapshots { get; set; }
+    /// <summary>
+    ///     Copies the values and <see cref="IZfsProperty.IsLocal" /> property of all fields changed for the current object from
+    ///     <see cref="BaseDataset" /> to <see cref="TreeDataset" />, effectively resetting <see cref="TreeDataset" /> to its original
+    ///     state.
+    /// </summary>
+    /// <param name="clearModifiedPropertiesCollection"></param>
+    public void CopyBaseDatasetPropertiesToTreeDataset( bool clearModifiedPropertiesCollection = true )
+    {
+        foreach ( string propName in _modifiedPropertiesSinceLastSave.Keys )
+        {
+            switch ( BaseDataset[ propName ] )
+            {
+                case ZfsProperty<bool> prop:
+                    TreeDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+                case ZfsProperty<int> prop:
+                    TreeDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+                case ZfsProperty<DateTimeOffset> prop:
+                    TreeDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+                case ZfsProperty<string> prop:
+                    TreeDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+            }
+        }
 
+        if ( clearModifiedPropertiesCollection )
+        {
+            _modifiedPropertiesSinceLastSave.Clear( );
+        }
+    }
+
+    /// <summary>
+    ///     Copies the values and <see cref="IZfsProperty.IsLocal" /> property of all fields changed for the current object from
+    ///     <see cref="TreeDataset" /> to <see cref="BaseDataset" />.<br />
+    ///     This method should be called after saving an object, to avoid needing a refresh from ZFS.
+    /// </summary>
+    /// <param name="clearModifiedPropertiesCollection"></param>
+    public void CopyTreeDatasetPropertiesToBaseDataset( bool clearModifiedPropertiesCollection = true )
+    {
+        foreach ( string propName in _modifiedPropertiesSinceLastSave.Keys )
+        {
+            switch ( TreeDataset[ propName ] )
+            {
+                case ZfsProperty<bool> prop:
+                    BaseDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+                case ZfsProperty<int> prop:
+                    BaseDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+                case ZfsProperty<DateTimeOffset> prop:
+                    BaseDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+                case ZfsProperty<string> prop:
+                    BaseDataset.UpdateProperty( propName, prop.Value, prop.IsLocal );
+                    continue;
+            }
+        }
+
+        if ( clearModifiedPropertiesCollection )
+        {
+            _modifiedPropertiesSinceLastSave.Clear( );
+        }
+    }
+
+    /// <inheritdoc />
+    public override string ToString( )
+    {
+        return TreeDataset.Name.GetLastPathElement( );
+    }
+
+    /// <summary>
+    ///     Updates a boolean property of the <see cref="TreeDataset" />, tracks the modified property, and returns a reference to the
+    ///     newly-updated property.
+    /// </summary>
+    /// <param name="propertyName"></param>
+    /// <param name="propertyValue"></param>
+    /// <returns>
+    ///     A <see langword="ref" /> <see cref="ZfsProperty{T}" /> where T is <see langword="bool" />, referring to the newly-updated
+    ///     property
+    /// </returns>
     public ref readonly ZfsProperty<bool> UpdateTreeNodeProperty( string propertyName, bool propertyValue )
     {
-        _modifiedPropertiesSinceLastSaveForCurrentItem[ propertyName ] = new ZfsProperty<bool>( propertyName, propertyValue, ZfsPropertySourceConstants.Local );
+        _modifiedPropertiesSinceLastSave[ propertyName ] = new ZfsProperty<bool>( TreeDataset, propertyName, propertyValue );
         return ref TreeDataset.UpdateProperty( propertyName, propertyValue );
     }
 
+    /// <summary>
+    ///     Updates an integer property of the <see cref="TreeDataset" />, tracks the modified property, and returns a reference to the
+    ///     newly-updated property.
+    /// </summary>
+    /// <param name="propertyName"></param>
+    /// <param name="propertyValue"></param>
+    /// <returns>
+    ///     A <see langword="ref" /> <see cref="ZfsProperty{T}" /> where T is <see langword="int" />, referring to the newly-updated
+    ///     property
+    /// </returns>
     public ref readonly ZfsProperty<int> UpdateTreeNodeProperty( string propertyName, int propertyValue )
     {
-        _modifiedPropertiesSinceLastSaveForCurrentItem[ propertyName ] = new ZfsProperty<int>( propertyName, propertyValue, ZfsPropertySourceConstants.Local );
+        _modifiedPropertiesSinceLastSave[ propertyName ] = new ZfsProperty<int>( TreeDataset, propertyName, propertyValue );
         return ref TreeDataset.UpdateProperty( propertyName, propertyValue );
     }
 
     public ref readonly ZfsProperty<DateTimeOffset> UpdateTreeNodeProperty( string propertyName, DateTimeOffset propertyValue )
     {
-        _modifiedPropertiesSinceLastSaveForCurrentItem[ propertyName ] = new ZfsProperty<DateTimeOffset>( propertyName, propertyValue, ZfsPropertySourceConstants.Local );
+        _modifiedPropertiesSinceLastSave[ propertyName ] = new ZfsProperty<DateTimeOffset>( TreeDataset, propertyName, propertyValue );
         return ref TreeDataset.UpdateProperty( propertyName, propertyValue );
     }
 
-    public IZfsProperty UpdateTreeNodeProperty( string propertyName, string propertyValue )
+    public ref readonly ZfsProperty<string> UpdateTreeNodeProperty( string propertyName, string propertyValue )
     {
-        _modifiedPropertiesSinceLastSaveForCurrentItem[ propertyName ] = new ZfsProperty<string>( propertyName, propertyValue, ZfsPropertySourceConstants.Local );
-        return TreeDataset.UpdateProperty( propertyName, propertyValue );
+        _modifiedPropertiesSinceLastSave[ propertyName ] = new ZfsProperty<string>( TreeDataset, propertyName, propertyValue );
+        return ref TreeDataset.UpdateProperty( propertyName, propertyValue );
     }
-
-    public ref readonly ZfsProperty<string> UpdateTreeNodeStringProperty( string propertyName, string propertyValue )
-    {
-        _modifiedPropertiesSinceLastSaveForCurrentItem[ propertyName ] = new ZfsProperty<string>( propertyName, propertyValue, ZfsPropertySourceConstants.Local );
-        return ref TreeDataset.UpdateStringProperty( propertyName, propertyValue );
-    }
-    /// <summary>
-    ///     Performs a depth-first recursion on the entire tree, updating all boolean properties appropriately
-    /// </summary>
-    /// <param name="currentNode"></param>
-    /// <param name="prop"></param>
-    /// <param name="source"></param>
-    private static void UpdateDescendentsBooleanPropertyInheritance( ZfsObjectConfigurationTreeNode currentNode, ZfsProperty<bool> prop, string source )
-    {
-        foreach ( ZfsObjectConfigurationTreeNode child in currentNode.Children.Cast<ZfsObjectConfigurationTreeNode>( ).Where( child => !child.TreeDataset[ prop.Name ].IsLocal ) )
-        {
-            UpdateDescendentsBooleanPropertyInheritance( child, prop, source );
-        }
-
-        // This is the final base case, for the node we started from
-        if ( currentNode.TreeDataset[ prop.Name ].IsLocal )
-        {
-            return;
-        }
-
-        // For everyone that makes it this far, we need to inherit, so update the tree and base copies of the property
-        currentNode.TreeDataset.UpdateProperty( prop.Name, prop.Value, source );
-        currentNode.BaseDataset.UpdateProperty( prop.Name, prop.Value, source );
-    }
-
 
     internal List<IZfsProperty> GetModifiedZfsProperties( )
     {
-        return _modifiedPropertiesSinceLastSaveForCurrentItem.Values.ToList( );
+        return _modifiedPropertiesSinceLastSave.Values.ToList( );
     }
-
-    private void RestorePreviousSelectedZfsTreeNode( )
-    {
-        foreach ( string propName in _modifiedPropertiesSinceLastSaveForCurrentItem.Keys )
-        {
-            switch ( BaseDataset[propName] )
-            {
-                case ZfsProperty<bool> prop:
-                    TreeDataset.UpdateProperty( propName, prop.Value, prop.Source );
-                    continue;
-                case ZfsProperty<int> prop:
-                    TreeDataset.UpdateProperty( propName, prop.Value, prop.Source );
-                    continue;
-                case ZfsProperty<DateTimeOffset> prop:
-                    TreeDataset.UpdateProperty( propName, prop.Value, prop.Source );
-                    continue;
-                case ZfsProperty<string> prop:
-                    TreeDataset.UpdateProperty( propName, prop.Value, prop.Source );
-                    continue;
-            }
-        }
-
-        _modifiedPropertiesSinceLastSaveForCurrentItem.Clear( );
-    }
-
 }
