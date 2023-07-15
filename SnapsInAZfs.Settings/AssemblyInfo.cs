e65f939e616b474b1dc65abe716154ef17b3686a
Brandon Thetford <github@snapsinazfs.com>
2023-07-15 04:27:03 -0700

    This isn't working properly - put it in a cs file

---
+++ SnapsInAZfs.Settings/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// LICENSE:
+// 
+// This software is licensed for use under the Free Software Foundation's GPL v3.0 license
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo( "SnapsInAZfs.Settings.Tests" )]
