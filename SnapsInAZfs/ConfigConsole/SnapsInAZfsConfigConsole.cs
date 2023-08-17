dfd5134888970d47600d26f233a31eebd62871b5
Brandon Thetford <github@snapsinazfs.com>
2023-08-16 18:37:15 -0700

    Run cleanup rules

--- SnapsInAZfs/ConfigConsole/SnapsInAZfsConfigConsole.cs
+++ SnapsInAZfs/ConfigConsole/SnapsInAZfsConfigConsole.cs
@@ -2,11 +2,11 @@
 
 // Copyright 2023 Brandon Thetford
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the �Software�), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // 
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED �AS IS�, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // 
 // See https://opensource.org/license/MIT/
 
@@ -128,36 +128,6 @@ public sealed partial class SnapsInAZfsConfigConsole
         zfsConfigMenuItem.Action = ShowZfsConfigurationWindow;
         zfsConfigMenuItem.Title = "Show _Template Configuration Window";
         _zfsConfigurationWindowShown = false;
-    }
-
-    private void ShowGlobalConfigurationWindow( )
-    {
-        if ( _templateConfigurationWindowShown )
-        {
-            HideTemplateConfigurationWindow( );
-        }
-
-        if ( _zfsConfigurationWindowShown )
-        {
-            HideZfsConfigurationWindow( );
-        }
-
-        _globalConfigurationWindow ??= new( );
-        Add( _globalConfigurationWindow );
-        if ( ShowChild( _globalConfigurationWindow ) )
-        {
-            LayoutSubviews( );
-            _globalConfigurationWindowShown = true;
-            _globalConfigurationWindow.dryRunRadioGroup.SetFocus( );
-            Logger.Debug( "Showing global configuration window" );
-            globalConfigMenuItem.Action = HideGlobalConfigurationWindow;
-            globalConfigMenuItem.Title = "Hide _Global Configuration Window";
-        }
-        else
-        {
-            Remove( _globalConfigurationWindow );
-            Logger.Error( "Unable to show global configuration window" );
-        }
     }
 
     private void SaveGlobalConfiguration( )
@@ -305,6 +275,36 @@ public sealed partial class SnapsInAZfsConfigConsole
                     return ( false, "error" );
                 }
             }
+        }
+    }
+
+    private void ShowGlobalConfigurationWindow( )
+    {
+        if ( _templateConfigurationWindowShown )
+        {
+            HideTemplateConfigurationWindow( );
+        }
+
+        if ( _zfsConfigurationWindowShown )
+        {
+            HideZfsConfigurationWindow( );
+        }
+
+        _globalConfigurationWindow ??= new( );
+        Add( _globalConfigurationWindow );
+        if ( ShowChild( _globalConfigurationWindow ) )
+        {
+            LayoutSubviews( );
+            _globalConfigurationWindowShown = true;
+            _globalConfigurationWindow.dryRunRadioGroup.SetFocus( );
+            Logger.Debug( "Showing global configuration window" );
+            globalConfigMenuItem.Action = HideGlobalConfigurationWindow;
+            globalConfigMenuItem.Title = "Hide _Global Configuration Window";
+        }
+        else
+        {
+            Remove( _globalConfigurationWindow );
+            Logger.Error( "Unable to show global configuration window" );
         }
     }
 
