1b63cf4e581db1ce41a5a1ef6149628c7095ae64
Brandon Thetford <github@snapsinazfs.com>
2023-08-16 18:36:08 -0700

    Combine this to a single switch for readability

--- SnapsInAZfs/ConfigConsole/SnapsInAZfsConfigConsole.cs
+++ SnapsInAZfs/ConfigConsole/SnapsInAZfsConfigConsole.cs
@@ -2,11 +2,11 @@
 
 // Copyright 2023 Brandon Thetford
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ï¿½Softwareï¿½), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // 
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED ï¿½AS ISï¿½, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // 
 // See https://opensource.org/license/MIT/
 
@@ -243,18 +243,15 @@ public sealed partial class SnapsInAZfsConfigConsole
 
             ( bool status, string reasonOrFile ) = ShowSaveDialog( newSettingsToSave );
 
-            if ( status )
+            switch ( status, reasonOrFile )
             {
-                Logger.Info( "Configuration saved to {0}", reasonOrFile );
-                return;
-            }
-
-            switch ( reasonOrFile )
-            {
-                case "canceled":
+                case (true, _):
+                    Logger.Info( "Configuration saved to {0}", reasonOrFile );
+                    return;
+                case (false, "canceled"):
                     Logger.Debug( "Canceled configuration save dialog" );
                     return;
-                case "no file name":
+                case (false, "no file name"):
                     Logger.Error( "No file name provided in save dialog. Configuration not saved." );
                     return;
                 default:
