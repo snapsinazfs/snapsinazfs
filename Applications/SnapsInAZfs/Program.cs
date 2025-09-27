71baf8b9d1104d2b78ed32e8196e9bba5d3be3f5
Brandon Thetford <github@snapsinazfs.com>
2025-09-26 22:43:43 -0700

    Initial basic replacement of PowerArgs (not functional yet)
    Just swaps out the call to powerargs with invoking the new code.
    Most of the implementation details still need to be re-created in SCL fashion and wired up, but the code in Main() is not likely to change much in the process.

--- Applications/SnapsInAZfs/Program.cs
+++ Applications/SnapsInAZfs/Program.cs
@@ -12,10 +12,12 @@
 
 namespace SnapsInAZfs;
 
+using System.CommandLine;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
+using CommandLine;
 using ConfigConsole;
 using Interop;
 using Interop.Zfs.ZfsCommandRunner;
@@ -30,6 +32,7 @@ using NLog.Config;
 using NLog.Extensions.Logging;
 using PowerArgs;
 using Settings.Logging;
+using SCL = System.CommandLine;
 
 [UsedImplicitly]
 internal static class Program
@@ -40,13 +43,34 @@ internal static class Program
     private static readonly IMonitor             ServiceObserver = new Monitor( );
     private static          IConfigurationRoot?  _configurationRoot;
     internal static         SnapsInAZfsSettings? Settings;
-
     internal static IZfsCommandRunner? ZfsCommandRunnerSingleton;
 
     [ExcludeFromCodeCoverage ( Justification = "Largely un-testable" )]
     public static async Task<int> Main( string[] argv )
     {
-        CommandLineArguments args = await Args.ParseAsync<CommandLineArguments> ( argv ).ConfigureAwait ( true );
+        if ( !ProcessCommandLine ( argv, out SCL.ParseResult siazCliParseResult, out Settings, out _configurationRoot,out ExitCode siazCliInvocationExitCode )
+          || siazCliInvocationExitCode is not ExitCode.EOK )
+        {
+            LogManager.Shutdown( );
+
+            return (int)siazCliInvocationExitCode;
+        }
+
+        switch ( siazCliParseResult )
+        {
+            case { RootCommandResult.IdentifierToken.Value: SiazCommandLine.RunCommandName }:
+                return Settings.Monitoring.EnableHttp switch
+                       {
+                           true => await RunWithKestrelAsync ( Settings, _configurationRoot ).ConfigureAwait ( true ),
+                           _    => await RunWithoutKestrelAsync ( Settings ).ConfigureAwait ( true )
+                       };
+            default:
+                break;
+        }
+
+        return 0;
+
+        CommandLineArguments args           = await Args.ParseAsync<CommandLineArguments> ( argv ).ConfigureAwait ( true );
 
         // Implicit null check here is important.
         if ( args is not { Help: false } )
@@ -115,6 +139,28 @@ internal static class Program
                    true => await RunWithKestrelAsync ( Settings, _configurationRoot ).ConfigureAwait ( true ),
                    _    => await RunWithoutKestrelAsync ( Settings ).ConfigureAwait ( true )
                };
+    }
+
+    private static bool ProcessCommandLine(
+        string[] arguments,
+        out SCL.ParseResult siazCliParseResult,
+        [NotNullWhen ( true )] out SnapsInAZfsSettings? settings,
+        [NotNullWhen ( true )] out IConfigurationRoot? configurationRoot,
+        out ExitCode exitCode
+        )
+    {
+        SiazCommandLine siazCli = new ( );
+        siazCliParseResult = siazCli.Parse ( arguments, out RootCommand _, new ( ) { EnablePosixBundling = true } );
+        exitCode = siazCli.Invoke (
+                                 arguments,
+                                 out RootCommand _,
+                                 out siazCliParseResult,
+                                 out settings,
+                                 out configurationRoot,
+                                 parserConfiguration: new ( ) { EnablePosixBundling = true }
+                                );
+
+        return exitCode == ExitCode.EOK;
     }
 
     /// <summary>
