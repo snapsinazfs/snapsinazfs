7d28a2f4283bd676e3f808811c859c12f1e7ef45
Brandon Thetford <github@snapsinazfs.com>
2025-10-06 10:39:42 -0700

    Add some ANSI string constants for use in raw literals.

---
+++ Applications/SnapsInAZfs/StringFormattingConstants.cs
@@ -0,0 +1,24 @@
+// Copyright $CurrentDate.Year Brandon Thetford
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// See https://opensource.org/license/MIT/
+
+namespace SnapsInAZfs;
+
+internal static class StringFormattingConstants
+{
+  internal const string U        = "\e[4m";
+  internal const string _U       = "\e[24m";
+  internal const string B        = "\e[1m";
+  internal const string _B       = "\e[22m";
+  internal const string FGYELLOW = "\e[93m";
+  internal const string _FGCOLOR = "\e[39m";
+  internal const string N        = "\e[7m";
+  internal const string P        = "\e[27m";
+  internal const string FGRED    = "\e[91m";
+}
