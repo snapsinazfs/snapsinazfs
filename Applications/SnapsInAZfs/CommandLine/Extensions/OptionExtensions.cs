fd4478135f62b63dd6e598cf4fe88e1f84fbfe69
Brandon Thetford <github@snapsinazfs.com>
2025-10-12 04:06:53 -0700

    Improve sorting a bit and add XmlDoc comments.

--- Applications/SnapsInAZfs/CommandLine/Extensions/OptionExtensions.cs
+++ Applications/SnapsInAZfs/CommandLine/Extensions/OptionExtensions.cs
@@ -15,10 +15,8 @@ namespace SnapsInAZfs.CommandLine.Extensions;
 using System.CommandLine;
 using System.CommandLine.Completions;
 using System.CommandLine.Parsing;
-using System.ComponentModel;
 using System.Globalization;
 using System.Numerics;
-using System.Reflection.Emit;
 
 /// <summary>
 ///   Extension methods for <see cref="Option{T}" />, enabling fluent usage.
@@ -26,72 +24,138 @@ using System.Reflection.Emit;
 [PublicAPI]
 public static class OptionExtensions
 {
-  /// <summary>
-  ///   Adds the provided <paramref name="validator" /> delegate to the <see cref="Option.Validators" /> collection for the current
-  ///   <see cref="Option{T}" />
-  /// </summary>
-  /// <typeparam name="TOption">The type of the option.</typeparam>
-  /// <param name="option">The option to which <paramref name="validator" /> will be added.</param>
-  /// <param name="validator">
-  ///   An <see cref="Action{T}" /> accepting an <see cref="OptionResult" /> and returning nothing, which will be called on every
-  ///   value of <paramref name="option" />.
+  /// <param name="option">
+  ///   The <see cref="Option{T}" /> to which the operation applies.
   /// </param>
-  /// <returns>
-  ///   A reference to <paramref name="option" />, after appending <paramref name="validator" />.
-  /// </returns>
-  public static Option<TOption> WithValidator<TOption> ( this Option<TOption> option, Action<OptionResult> validator )
-  {
-    option.Validators.Add ( validator );
-
-    return option;
-  }
-
+  /// <typeparam name="T">The type of the option value.</typeparam>
   extension<T> ( Option<T> option )
   {
-    public Option<T> WithCompletionSource ( Func<CompletionContext,IEnumerable<CompletionItem>> completionItemFactory )
+    /// <summary>
+    ///   Adds a source of <see cref="CompletionItem" />s to the <paramref name="option" /> and returns a reference to
+    ///   <paramref name="option" />.
+    /// </summary>
+    /// <param name="completionItemFactory">
+    ///   A delegate that accepts a <see cref="CompletionContext" /> and returns an <see cref="IEnumerable{T}" /> of
+    ///   <see cref="CompletionItem" />s.
+    /// </param>
+    /// <returns>The same option that this method was called on.</returns>
+    public Option<T> WithCompletionSource ( Func<CompletionContext, IEnumerable<CompletionItem>> completionItemFactory )
     {
-      option.CompletionSources.Add( completionItemFactory );
+      option.CompletionSources.Add ( completionItemFactory );
+      return option;
+    }
+
+    /// <summary>
+    ///   Adds the provided <paramref name="validator" /> delegate to the <see cref="Option.Validators" /> collection for the current
+    ///   <see cref="Option{T}" />
+    /// </summary>
+    /// <param name="validator">
+    ///   An <see cref="Action{T}" /> accepting an <see cref="OptionResult" /> and returning nothing, which will be called on every
+    ///   value of <paramref name="option" />.
+    /// </param>
+    /// <returns>
+    ///   A reference to <paramref name="option" />, after appending <paramref name="validator" />.
+    /// </returns>
+    public Option<T> WithValidator ( Action<OptionResult> validator )
+    {
+      option.Validators.Add ( validator );
+
       return option;
     }
   }
 
-
+  /// <param name="option">
+  ///   The <see cref="Option{T}" /> where <typeparamref name="T" /> is <see cref="IBinaryInteger{T}" /> of <typeparamref name="T" />
+  ///   to which the operation applies.
+  /// </param>
   extension<T> ( Option<T> option )
-  where T : IBinaryInteger<T>
+    where T : IBinaryInteger<T>
   {
+    /// <summary>
+    ///   For a given <see cref="Option{T}" /> of <see langword="int" /> type, adds suggested values, used for tab completion and help
+    ///   text, with sorting in numeric order.
+    /// </summary>
+    /// <param name="values">
+    ///   One or more <see langword="int" /> values to provide as suggestions and tab completions.
+    /// </param>
+    /// <remarks>This method DOES NOT restrict entry to the specified values.</remarks>
+    /// <returns>The same option that this method was called on.</returns>
     public Option<T> WithSuggestedCompletionValues ( params int[] values )
     {
-      option.CompletionSources.Add ( c =>
-                                     {
-                                       return values.Select ( static i => new CompletionItem ( label: i.ToString ( "D", CultureInfo.CurrentCulture ), sortText: i.ToString ( "D10", CultureInfo.CurrentCulture ) ) ).ToArray ( );
-                                     }
-                                   );
+      option.CompletionSources
+            .Add ( _ =>
+                     values
+                      .Select
+                         ( static i =>
+                             new CompletionItem (
+                                                 i.ToString ( "D",             CultureInfo.CurrentCulture ),
+                                                 sortText: i.ToString ( "D10", CultureInfo.CurrentCulture )
+                                                )
+                         )
+                      .ToArray ( )
+                 );
       return option;
     }
   }
-  
+
+  /// <param name="option">
+  ///   The <see cref="Option{T}" /> where <typeparamref name="T" /> is an <see langword="unmanaged" /> <see cref="Enum" /> to which
+  ///   the operation applies.
+  /// </param>
+  /// <typeparam name="T">The type of the option value.</typeparam>
   extension<T> ( Option<T> option )
-  where T : unmanaged, Enum
+    where T : unmanaged, Enum
   {
+    /// <summary>
+    ///   For a given <see cref="Option{T}" /> of <see langword="enum" /> type, adds suggested values from the enum names, sorted by
+    ///   their values.
+    /// </summary>
+    /// <returns>The same option that this method was called on.</returns>
     public Option<T> WithValueOrderedEnumHelpText ( )
     {
       option.HelpName = string.Join ( '|', Enum.GetValues<T> ( ).Select ( static e => e.ToString ( "G" ) ) );
       return option;
     }
   }
-  
-  extension<T> ( T enumType )
-  where T : struct, Enum
+
+  /// <param name="option">
+  ///   The <see cref="Option{T}" /> where <typeparamref name="T" /> is an <see langword="unmanaged" /> <see cref="Enum" /> with
+  ///   underlying type <see cref="IBinaryInteger{T}" /> of <typeparamref name="T" /> to which the operation applies.
+  /// </param>
+  /// <typeparam name="T">The type of the option value.</typeparam>
+  extension<T> ( Option<T> option )
+    where T : unmanaged, Enum, IBinaryInteger<T>
   {
+    /// <summary>
+    ///   For a given <see cref="Option{T}" /> of <see langword="enum" /> type, gets an ordered collection of
+    ///   <see cref="CompletionItem" />s, sorted by numeric value.
+    /// </summary>
+    /// <remarks>
+    ///   Values are treated as 20-digit decimal numbers for sorting, to support the maximum possible length of a signed 64-bit value.
+    /// </remarks>
+    /// <returns>
+    ///   An ordered collection of <see cref="CompletionItem" />s, sorted by numeric value.
+    /// </returns>
     public IEnumerable<CompletionItem> GetOrderedEnumCompletionItems ( )
     {
-      
-      return Enum.GetValues<T> ( ).Select ( static e => new CompletionItem ( e.ToString ( "G" ), sortText: e.ToString ( "D" ) ) );
+      return Enum.GetValues<T> ( ).Select ( static e => new CompletionItem ( e.ToString ( "G" ), sortText: $"{Convert.ToInt64 ( e, NumberFormatInfo.CurrentInfo ):D20}" ) );
     }
   }
 
+  /// <param name="level">
+  ///   The logging level to get the <see langword="string" /> name of without reflection.
+  /// </param>
   extension ( LoggingLevel level )
   {
+    // False positive
+    // TODO: Remove this when fixed version of .net is released.
+#pragma warning disable CS1734
+    /// <summary>
+    ///   Reflection-free conversion of <see cref="LoggingLevel" /> values to their <see langword="string" /> representations.
+    /// </summary>
+    /// <returns>The <see langword="string" /> representation of <paramref name="level" />.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">If <paramref name="level" /> is set to an undefined value.</exception>
+#pragma warning restore CS1734
     public string ToNameString ( )
     {
       return level switch
@@ -107,5 +171,4 @@ public static class OptionExtensions
              };
     }
   }
-  
 }
