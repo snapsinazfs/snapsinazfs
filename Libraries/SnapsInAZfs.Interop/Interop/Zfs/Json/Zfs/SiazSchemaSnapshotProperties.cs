62700921f0bd10306b50b73db42f76c11cb7927c
Brandon Thetford <github@snapsinazfs.com>
2025-10-25 01:05:40 -0700

    Fix naming

--- Libraries/SnapsInAZfs.Interop/Interop/Zfs/Json/Zfs/SiazSchemaSnapshotProperties.cs
+++ Libraries/SnapsInAZfs.Interop/Interop/Zfs/Json/Zfs/SiazSchemaSnapshotProperties.cs
@@ -2,16 +2,17 @@
 namespace SnapsInAZfs.Interop.Zfs.Json.Zfs;
 
 using System.Text.Json.Serialization;
+using ZfsTypes;
 
 [PublicAPI]
 public record SiazSchemaSnapshotProperties : SiazSchemaDatasetProperties
 {
-  [JsonPropertyName ( "snapsinazfs.com:snapshot:period" )]
-  public DatasetProperty<string>? snapshotPeriod { get; set; }
+  [JsonPropertyName ( ZfsPropertyNames.SnapshotPeriod )]
+  public DatasetProperty<string>? SnapshotPeriod { get; set; }
 
-  [JsonPropertyName ( "snapsinazfs.com:snapshot:timestamp" )]
-  public DatasetProperty<DateTimeOffset>? snapshotTimestamp { get; set; }
+  [JsonPropertyName ( ZfsPropertyNames.SnapshotTimestamp )]
+  public DatasetProperty<DateTimeOffset>? SnapshotTimestamp { get; set; }
 
   [JsonPropertyName ( "userrefs" )]
-  public DatasetProperty<int> UserRefs { get; set; }
+  public DatasetProperty<int>? UserRefs { get; set; }
 }
